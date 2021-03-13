e8ad6a9d9fe548b2e677648ab9c8a7caf40495ba
JUN HO <43059067+PYUPIT@users.noreply.github.com>
2021-03-13 23:50:09 +0900

    Study BFS(Breadth First Search)

---
+++ 자료구조/bfs.py
@@ -0,0 +1,36 @@
+from collections import deque
+
+def bfs(graph, start, visited):
+
+    que = deque([start])
+    visited[start] = True
+
+    print(que)
+
+    while que:
+
+        v = que.popleft()
+        print(v, end = ' ')
+
+        for x in graph[v]:
+            
+            if not visited[x]:
+                que.append(x)
+                visited[x] = True
+
+
+graph = [
+    [],
+    [2, 3, 8],
+    [1, 7],
+    [1, 4, 5],
+    [3, 5],
+    [3, 4],
+    [7],
+    [2, 6, 8],
+    [1, 7]
+]
+
+visited = [False] * 9
+
+bfs(graph, 1, visited)
