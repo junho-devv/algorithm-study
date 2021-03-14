2f1e49d4cd018a48372538ae42fa8ff1f7f38880
JUN HO <43059067+PYUPIT@users.noreply.github.com>
2021-03-14 23:53:12 +0900

    Study search problem

--- 자료구조/search ex001.py
+++ 자료구조/search ex001.py
@@ -2,9 +2,25 @@ n, m = map(int, input().split())
 
 graph = []
 for x in range(n):
-    graph.append(list(map(int, input().split()))
+    graph.append(list(map(int, input().split())))
+
 
 def dfs(x, y):
+
+    if x <= -1 or x >= n or y <= -1 or y >= n:
+        return False
+    
+    if graph[x][y] == 0:
+
+        graph[x][y] = 1
+
+        dfs(x-1, y)
+        dfs(x+1, y)
+        dfs(x, y-1)
+        dfs(x, y+1)
+
+        return True
+
     return False
 
 
