02c31e7b48c24a270fd82253b8f8841506052e86
Junho Jin <junho.devv@gmail.com>
2021-03-19 23:40:28 +0900

    Study Sorting Algorithm

---
+++ 정렬_ex003.py
@@ -0,0 +1,19 @@
+n, k = map(int, input().split())
+
+aList = [1, 3, 5, 2, 4]
+bList = [5, 5, 6, 6, 5]
+
+aList.sort()
+bList.sort(reverse=True)
+
+for i in range(k):
+    if aList[i] >= bList[i]:
+        continue
+    else:
+        aList[i], bList[i] = bList[i], aList[i]
+
+list_sum = 0
+for i in aList:
+    list_sum += i
+
+print(list_sum)
