a243c199a2209f0aae14e42e546754c9665f2acd
Junho Jin <junho.devv@gmail.com>
2021-03-23 23:24:45 +0900

    Study Dynamic Programming

---
+++ PRACTICE/DP_ex002.py
@@ -0,0 +1,18 @@
+n = int(input())
+fList = list(map(int, input().split()))
+
+dp_table = [0] * n
+
+
+def rob_foods(num):
+    dp_table[0] = fList[0]
+    dp_table[1] = max(fList[0], fList[1])
+
+    for x in range(2, num):
+        dp_table[x] = max(dp_table[x-2] + fList[x], dp_table[x-1])
+
+    return dp_table[num-1]
+
+
+aResult = rob_foods(n)
+print(aResult)
