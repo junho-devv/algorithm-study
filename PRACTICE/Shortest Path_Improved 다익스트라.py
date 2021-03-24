d6a1ba610ca3ac951ab526beef5aafe6e72d0179
Junho Jin <junho.devv@gmail.com>
2021-03-24 23:11:22 +0900

    Study Shortest Path Algorithm

---
+++ PRACTICE/Shortest Path_Improved 다익스트라.py
@@ -0,0 +1,44 @@
+import heapq
+
+INF = int(1e9)
+
+node, line = map(int, input().split())
+
+start_node = int(input())
+
+graph = [[] for _ in range(node+1)]
+
+distance = [INF] * (node+1)
+
+for _ in range(line):
+    a, b, c = map(int, input().split())
+    graph[a].append([b, c])
+
+
+def improved_dijkstra(start):
+    que = []
+    heapq.heappush(que, (0, start))
+    distance[start] = 0
+
+    while que:
+        node_dist, now = heapq.heappop(que)
+
+        if distance[now] < node_dist:
+            continue
+
+        for x in graph[now]:
+            cost = node_dist + x[1]
+
+            if cost < distance[x[0]]:
+                distance[x[0]] = cost
+                heapq.heappush(que, (cost, x[0]))
+
+
+improved_dijkstra(start_node)
+
+for x in range(node+1):
+
+    if distance[x] == INF:
+        print("무한")
+    else:
+        print(distance[x])
