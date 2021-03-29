d0f1c005f6128972315c91d0555db9b7e8266a5b
Junho Jin <junho.devv@gmail.com>
2021-03-29 23:36:42 +0900

    Study Topology Sort

---
+++ PRACTICE/MST_위상정렬.py
@@ -0,0 +1,39 @@
+from collections import deque
+
+n_num, e_num = map(int, input().split())
+
+# in-degree(진입차수)
+in_degree = [0] * (n_num + 1)
+graph = [[] for _ in range(n_num + 1)]
+
+for _ in range(e_num):
+    a_node, b_node = map(int, input().split())
+    graph[a_node].append(b_node)
+    in_degree[b_node] += 1
+
+
+def topology_sort():
+
+    a_result = []
+    a_que = deque()
+
+    for i in range(1, n_num + 1):
+        if in_degree[i] == 0:
+            a_que.append(i)
+
+    while a_que:
+
+        now = a_que.popleft()
+        a_result.append(now)
+
+        for i in graph[now]:
+            in_degree[i] -= 1
+
+            if in_degree[i] == 0:
+                a_que.append(i)
+
+    for i in a_result:
+        print(i, end=' ')
+
+
+topology_sort()
