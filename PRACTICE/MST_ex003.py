dd0ba80332546c3f133d77903d7ed4fec8d588f2
Junho Jin <junho.devv@gmail.com>
2021-04-02 00:35:02 +0900

    Study MST

--- PRACTICE/MST_ex003.py
+++ PRACTICE/MST_ex003.py
@@ -1,23 +1,29 @@
 from collections import deque
+import copy
 
 # 강의의 수 : c_num
 c_num = int(input())
 
-course_list = [[] for _ in range(c_num + 1)]
-
-for i in range(1, c_num + 1):
-    course_list[i].append(list(map(int, input().split())))
-
-print(course_list)
-
 in_order = [0] * (c_num + 1)
 
+course_list = [[] for _ in range(c_num + 1)]
+
+cost_list = [0] * (c_num + 1)
+
 for i in range(1, c_num + 1):
-    in_order[i] = len(course_list[i]) - 2
+    a_input = list(map(int, input().split()))
+    cost_list[i] = a_input[0]
+    for j in a_input[1:-1]:
+        in_order[i] += 1
+        course_list[j].append(i)
+
+print(course_list)
+print(cost_list)
+
 
 def topology_sort():
 
-    a_result = []
+    a_result = copy.deepcopy(cost_list)
     a_que = deque()
 
     for x in range(1, c_num + 1):
@@ -27,15 +33,19 @@ def topology_sort():
     while a_que:
 
         now = a_que.popleft()
-        a_result.append(now)
 
-        for x in course_list[now]
+        for x in course_list[now]:
+
+            a_result[x] = max(a_result[x], cost_list[x] + a_result[now])
+
+            in_order[x] -= 1
+
+            if in_order[x] == 0:
+                a_que.append(x)
+
+    for x in range(1, c_num + 1):
+        print(a_result[x])
 
 
+topology_sort()
 
-# 5
-# 10 -1
-# 10 1 -1
-# 4 1 -1
-# 4 3 1 -1
-# 3 3 -1
