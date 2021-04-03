816264c110f3736a547500785e87d37cca28c1d9
Junho Jin <junho.devv@gmail.com>
2021-04-03 23:56:21 +0900

    Solve Algorithm Problem

--- PRACTICE/Practice_ex002.py
+++ PRACTICE/Practice_ex002.py
@@ -5,14 +5,17 @@ max_num[0] = int(n_stn[0])
 
 for i in range(1, len(n_stn)):
 
+    sss = int(n_stn[i])
+
     if max_num[i-1] == 0:
-        max_num[i] = int(n_stn[i])
+        max_num[i] = sss
         continue
 
-    if int(n_stn[i]) == 0 or int(n_stn[i]) == 1:
-        max_num[i] = int(n_stn[i]) + max_num[i-1]
+    if sss == 0 or sss == 1:
+        max_num[i] = sss + max_num[i-1]
 
     else:
-        max_num[i] = int(n_stn[i]) * max_num[i-1]
+        max_num[i] = sss * max_num[i-1]
 
 print(max_num[-1])
+
