cc4a737f72d08fcbb96eb5b0c0554cf2a54b4583
Junho Jin <junho.devv@gmail.com>
2021-04-11 23:32:21 +0900

    Solve Algorithm Problem

--- PRACTICE/구현_ex007.py
+++ PRACTICE/구현_ex007.py
@@ -1,3 +1,5 @@
+from itertools import combinations
+
 n, m = map(int, input().split())
 
 city_map = []
@@ -5,4 +7,49 @@ city_map = []
 for i in range(n):
     city_map.append(list(map(int, input().split())))
 
-print(city_map)
+
+def calculate_distance(city_list, chicken_list):
+
+    answer = 0
+
+    for city in city_list:
+
+        result = int(1e9)
+
+        for chicken in chicken_list:
+
+            x_temp = abs(city[0] - chicken[0])
+            y_temp = abs(city[1] - chicken[1])
+
+            result = min(result, x_temp + y_temp)
+
+        answer += result
+
+    return answer
+
+
+def solution():
+
+    city_list = []
+    chicken_list = []
+
+    for x in range(n):
+        for y in range(n):
+
+            if city_map[x][y] == 1:
+                city_list.append([x, y])
+
+            if city_map[x][y] == 2:
+                chicken_list.append([x, y])
+
+    candidates = list(combinations(chicken_list, m))
+
+    answer = int(1e9)
+
+    for candidate in candidates:
+        answer = min(answer, calculate_distance(city_list, candidate))
+
+    return answer
+
+
+print(solution())
---
+++ PRACTICE/구현_ex008.py
@@ -0,0 +1,11 @@
+# 외벽 점검
+
+wall_length = int(input())
+
+# weak_list = list(map(int, input().split()))
+# dist_list = list(map(int, input().split()))
+
+weak_list = [1, 5, 6, 10]
+dist_list = [1, 2, 3, 4]
+
+
