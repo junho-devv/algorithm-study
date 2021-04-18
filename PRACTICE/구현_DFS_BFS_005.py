00d0083b8639c3fc2b3664893477998b86dea63b
Junho Jin <junho.devv@gmail.com>
2021-04-18 23:21:08 +0900

    Solve Algorithm Problem

--- PRACTICE/구현_DFS_BFS_005.py
+++ PRACTICE/구현_DFS_BFS_005.py
@@ -5,38 +5,64 @@ from collections import deque
 
 n_num = int(input())
 n_list = list(map(int, input().split()))
+
 # 덧셈(+), 뺄셈(-), 곱셈(X), 나눗셈(/)
 o_num_list = list(map(int, input().split()))
-o_list = []
-
-for i in range(4):
-    if o_num_list[i] > 0:
-        for j in range(o_num_list[i]):
-            o_list.append(i)
-
-candidates = list(permutations(o_list, len(o_list)))
 
 result_list = []
 
 
-def solution(start, operator, next_val):
+def calculate_result(n_que, o_que):
 
-    if operator == 0:
-        start += next_val
-    if operator == 1:
-        start -= next_val
-    if operator == 2:
-        start *= next_val
-    if operator == 3:
-        if start > 0:
-            start //= next_val
-        else:
-            start *= -1
-            start //= next_val
-            start *= -1
+    answer = n_que.popleft()
 
-    return solution(start, )
+    while o_que:
+
+        a_operator = o_que.popleft()
+        a_next = n_que.popleft()
+
+        if a_operator == 0:
+            answer += a_next
+        if a_operator == 1:
+            answer -= a_next
+        if a_operator == 2:
+            answer *= a_next
+        if a_operator == 3:
+            if answer > 0:
+                answer //= a_next
+            else:
+                answer *= -1
+                answer //= a_next
+                answer *= -1
+
+    return answer
 
 
-print(solution(n_list[0], o_list[0], n_list[1]))
+def solution():
 
+    o_list = deque()
+    for i in range(4):
+        if o_num_list[i] > 0:
+            for j in range(o_num_list[i]):
+                o_list.append(i)
+
+    max_result = (-1) * int(1e9)
+    min_result = int(1e9)
+
+    candidates = list(permutations(o_list, len(o_list)))
+    for candidate in candidates:
+
+        n_que = deque()
+        for x in range(n_num):
+            n_que.append(n_list[x])
+
+        a_result = calculate_result(n_que, deque(candidate))
+
+        max_result = max(max_result, a_result)
+        min_result = min(min_result, a_result)
+
+    print(max_result)
+    print(min_result)
+
+
+solution()
