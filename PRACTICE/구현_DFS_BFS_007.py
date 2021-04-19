ceb271534b19b589a9ffba03b842690571c9874c
Junho Jin <junho.devv@gmail.com>
2021-04-19 23:24:53 +0900

    Solve Algorithm Problem

---
+++ PRACTICE/구현_DFS_BFS_007.py
@@ -0,0 +1,22 @@
+# 인구 이동
+c_size, l, r = 2, 20, 50
+p_map = [
+    [50, 30],
+    [20, 40]
+]
+
+x_direction = [1, 0, -1, 0]
+y_direction = [0, 1, 0, -1]
+
+
+def solution():
+
+    for x in range(c_size):
+        for y in range(c_size):
+
+
+
+    return
+
+
+solution()
