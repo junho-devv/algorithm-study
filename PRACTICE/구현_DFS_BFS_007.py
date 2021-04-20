c065a926cd5dda2a723c9cd08f110bbd79db3ecd
Junho Jin <junho.devv@gmail.com>
2021-04-20 23:31:49 +0900

    Solve Algorithm Problem

--- PRACTICE/구현_DFS_BFS_007.py
+++ PRACTICE/구현_DFS_BFS_007.py
@@ -1,5 +1,7 @@
 # 인구 이동
-c_size, l, r = 2, 20, 50
+from collections import deque
+
+c_size, a_left, a_right = 2, 20, 50
 p_map = [
     [50, 30],
     [20, 40]
@@ -9,14 +11,65 @@ x_direction = [1, 0, -1, 0]
 y_direction = [0, 1, 0, -1]
 
 
+def unite_countries(a, b, sector, a_union):
+
+    a_united = [(a, b)]
+
+    a_que = deque()
+    a_que.append((a, b))
+
+    a_union[a][b] = sector
+
+    a_population = p_map[a][b]
+    u_num = 1
+
+    while a_que:
+        x, y = a_que.popleft()
+        # 4가지 방향을 확인
+        for d in range(4):
+            next_x = x + x_direction[d]
+            next_y = y + y_direction[d]
+
+            if 0 <= next_x < c_size and 0 <= next_y < c_size and a_union[next_x][next_y] == -1:
+                a_diff = abs(p_map[next_x][next_y] - p_map[x][y])
+                if a_left <= a_diff <= a_right:
+                    a_que.append((next_x, next_y))
+                    a_union[next_x][next_y] = sector
+                    a_population += p_map[next_x][next_y]
+                    u_num += 1
+                    a_united.append((next_x, next_y))
+
+    for x, y in a_united:
+        p_map[x][y] = a_population // u_num
+
+    return u_num
+
+
 def solution():
+    answer = 0
 
-    for x in range(c_size):
-        for y in range(c_size):
+    while True:
+        print("while")
+        print(p_map)
+        a_union = [[-1] * c_size for _ in range(c_size)]
+        sector = 0
+
+        for x in range(c_size):
+            print("a for")
+            for y in range(c_size):
+                print("for num")
+                if a_union[x][y] == -1:
+                    unite_countries(x, y, sector, a_union)
+                    print(a_union)
+                    sector += 1
+
+        if sector == c_size * c_size:
+            print(sector)
+            break
+
+        answer += 1
+    print(a_union)
+    return answer
 
 
-
-    return
-
-
-solution()
+print(solution())
---
+++ PRACTICE/구현_DFS_BFS_008.py
@@ -0,0 +1,77 @@
+# 블록 이동하기
+from collections import deque
+
+b_size = 5
+board = [
+    [0, 0, 0, 1, 1],
+    [0, 0, 0, 1, 0],
+    [0, 1, 0, 1, 1],
+    [1, 1, 0, 0, 1],
+    [0, 0, 0, 0, 0]
+]
+
+a_robot = {(0, 0), (1, 0)}
+
+robot_left = (0, 0)
+robot_right = (1, 0)
+
+
+def get_next_pos(pos, temp_board):
+    next_pos = []
+    pos = list(pos)
+    left_x, left_y, right_x, right_y = pos[0][0], pos[0][1], pos[1][0], pos[1][1]
+
+    x_direction = [1, 0, -1, 0]
+    y_direction = [0, 1, 0, -1]
+
+    for d in range(4):
+        left_nx, left_ny = left_x + x_direction[d], left_y + y_direction[d]
+        right_nx, right_ny = right_x + x_direction[d], right_y + y_direction[d]
+
+        if temp_board[left_nx][left_ny] == 0 and temp_board[right_nx][right_ny] == 0:
+            next_pos.append({(left_nx, left_ny), (right_nx, right_ny)})
+
+    if left_x == right_x:
+        for i in [-1, 1]:
+            if temp_board[left_x + i][left_y] == 0 and temp_board[right_x + i][left_y] == 0:
+                next_pos.append({(left_x, left_y), (left_x + i, left_y)})
+                next_pos.append({(right_x, right_y), (right_x + i, right_y)})
+
+    if left_y == right_y:
+        for i in [-1, 1]:
+            if temp_board[left_x][left_y + i] == 0 and temp_board[right_x][right_y + i] == 0:
+                next_pos.append({(left_x, left_y), (left_x, left_y + i)})
+                next_pos.append({(right_x, right_y), (right_x, right_y + i)})
+
+    return next_pos
+
+
+def solution():
+    answer = 0
+
+    temp_board = [[1] * (b_size + 2) for _ in range(b_size + 2)]
+    for x in range(b_size):
+        for y in range(b_size):
+            temp_board[x + 1][y + 1] = board[x][y]
+
+    start_pos = {(1, 1), (1, 2)}
+    a_que = deque()
+    a_que.append((start_pos, answer))
+    visited_list = [a_robot]
+
+    while a_que:
+        a_pos, answer = a_que.popleft()
+
+        if (b_size, b_size) in a_pos:
+            return answer
+
+        for next_pos in get_next_pos(a_pos, temp_board):
+
+            if next_pos not in visited_list:
+                a_que.append((next_pos, answer + 1))
+                visited_list.append(next_pos)
+
+    return answer
+
+
+print(solution())
