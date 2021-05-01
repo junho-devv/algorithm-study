a376e69ec5963574a4d7ce29cc903ae529662f55
Junho Jin <junho.devv@gmail.com>
2021-05-01 23:18:47 +0900

    Solve Algorithm Problem

---
+++ PRACTICE/구현_다이나믹_ex002.py
@@ -0,0 +1,26 @@
+u_index = 12
+
+dynamic_table = [0, 1]
+num_u = 1
+a_val = 1
+
+while num_u < u_index:
+    # 값을 1씩 증가
+    a_val += 1
+
+    if a_val % 2 == 0:
+        if dynamic_table[a_val // 2] != 0:
+            dynamic_table.append(a_val)
+            num_u += 1
+    elif a_val % 3 == 0:
+        if dynamic_table[a_val // 3] != 0:
+            dynamic_table.append(a_val)
+            num_u += 1
+    elif a_val % 5 == 0:
+        if dynamic_table[a_val // 5] != 0:
+            dynamic_table.append(a_val)
+            num_u += 1
+    else:
+        dynamic_table.append(0)
+
+print(dynamic_table)
