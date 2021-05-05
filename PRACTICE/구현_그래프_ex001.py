c23e4a57e37f6459a288aca7f436ae56914c00b7
Junho Jin <junho.devv@gmail.com>
2021-05-05 23:35:55 +0900

    Solve Algorithm Problem

---
+++ PRACTICE/구현_그래프_ex001.py
@@ -0,0 +1,53 @@
+num_a, num_b = map(int, input().split())
+
+graph_a = []
+for _ in range(num_a):
+    graph_a.append(list(map(int, input().split())))
+# 여행 계획에 포함된 여행지의 번호들을 입력하기
+graph_b = list(map(int, input().split()))
+
+
+def find_parent_node(a_list, a_node):
+    if a_list[a_node] != a_node:
+        a_list[a_node] = find_parent_node(a_list, a_list[a_node])
+
+    return a_list[a_node]
+
+
+def union_nodes(a_list, a_node, b_node):
+
+    parent_a = find_parent_node(a_list, a_node)
+    parent_b = find_parent_node(a_list, b_node)
+
+    if parent_a < parent_b:
+        a_list[b_node] = parent_a
+    else:
+        a_list[a_node] = parent_b
+
+
+parents = [0] * (num_a + 1)
+for i in range(1, num_a + 1):
+    parents[i] = i
+
+for x in range(num_a):
+    for y in range(x, num_a):
+        if graph_a[x][y] == 1:
+            union_nodes(parents, x + 1, y + 1)
+
+pivot = parents[graph_b[0]]
+
+answer = "YES"
+for i in graph_b:
+    if parents[i] != pivot:
+        answer = "NO"
+        break
+
+print(answer)
+
+# 5 4
+# 0 1 0 1 1
+# 1 0 1 1 0
+# 0 1 0 0 0
+# 1 1 0 0 0
+# 1 0 0 0 0
+# 2 3 4 3
---
+++ PRACTICE/구현_그래프_ex002.py
@@ -0,0 +1,55 @@
+# 탑승구의 수(num_a) 입력하기
+num_a = int(input())
+# 비행기의 수(num_b) 입력하기
+num_b = int(input())
+
+docking = []
+for _ in range(num_b):
+    docking.append(int(input()))
+
+parents = [0] * (num_a + 1)
+for i in range(1, num_a + 1):
+    parents[i] = i
+
+
+def find_parent_node(a_list, a_node):
+    if a_list[a_node] != a_node:
+        a_list[a_node] = find_parent_node(a_list, a_list[a_node])
+    return a_list[a_node]
+
+
+def union_nodes(a_list, a_node, b_node):
+    parent_a = find_parent_node(a_list, a_node)
+    parent_b = find_parent_node(a_list, b_node)
+
+    if parent_a < parent_b:
+        a_list[a_node] = parent_a
+    else:
+        a_list[a_node] = parent_b
+
+
+answer = True
+for d in docking:
+    gate = find_parent_node(parents, d)
+    if parents[gate] != 0:
+        union_nodes(parents, gate, gate - 1)
+    else:
+        answer = False
+        break
+print(parents)
+if answer:
+    print("YES")
+else:
+    print("NO")
+
+# docking.sort()
+#
+# range_a = 0
+# answer = 0
+#
+# for i in range(num_a):
+#     range_a = docking[i]
+#     if answer < range_a:
+#         answer += 1
+#
+# print(answer)
