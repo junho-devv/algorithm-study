e4e1bd81536e1f6fffe261acb2a9856a275b6010
Junho Jin <junho.devv@gmail.com>
2021-05-08 21:43:19 +0900

    Solve Algorithm Problem

---
+++ PRACTICE/구현_그래프_ex003.py
@@ -0,0 +1,16 @@
+# 3665_최종순위
+
+def solution():
+    # 테스트 케이스의 개수(num_tc) 입력하기
+    num_tc = int(input())
+    for tc in range(num_tc):
+        # 팀의 수(num_t) 입력하기
+        num_t = int(input())
+        team_rank = list(map(int, input().split()))
+
+
+    answer = 0
+    return answer
+
+
+print(solution())
