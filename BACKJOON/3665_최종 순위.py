88f42d423918aad09747c65e119d084abd469a38
Junho Jin <junho.devv@gmail.com>
2021-05-11 21:37:35 +0900

    Solve Algorithm Problem

---
+++ BACKJOON/3665_최종 순위.py
@@ -0,0 +1,25 @@
+from collections import deque
+
+
+def solution():
+    # 테스트 케이스의 개수(num_tc) 입력받기
+    num_tc = int(input())
+    for tc in range(num_tc):
+        # 팀의 수(num_t) 입력받기
+        num_t = int(input())
+        # 각 팀의 순위(rank_t) 입력받기
+        rank_t = list(map(int, input().split()))
+        # 각 팀의 진입차수를 '0'으로 초기화
+        in_degree = [0] * (num_t + 1)
+        graph_r = [[False] * (num_t + 1) for _ in range(num_t + 1)]
+        for i in range(num_t - 1):
+            for j in range(i + 1, num_t):
+                graph_r[rank_t[i]][rank_t[j]] = True
+                in_degree[rank_t[j]] += 1
+        print(in_degree)
+
+    answer = []
+    return answer
+
+
+print(solution())
