54c6bf263e357459b7ed4df769c8576f0816f2f9
Junho Jin <junho.devv@gmail.com>
2021-05-14 21:42:18 +0900

    Solve Algorithm Problem

--- BACKJOON/3665_최종 순위.py
+++ BACKJOON/3665_최종 순위.py
@@ -16,8 +16,23 @@ def solution():
             for j in range(i + 1, num_t):
                 graph_r[rank_t[i]][rank_t[j]] = True
                 in_degree[rank_t[j]] += 1
-        print(in_degree)
-    print(num_tc)
+        # 상대적 등수가 바뀐 팀의 수
+        num_c = int(input())
+        for _ in range(num_c):
+            a, b = map(int, input().split())
+
+            if graph_r[a][b]:
+                graph_r[a][b] = False
+                graph_r[b][a] = True
+                in_degree[b] -= 1
+                in_degree[a] += 1
+            else:
+                graph_r[a][b] = True
+                graph_r[b][a] = False
+                in_degree[a] -= 1
+                in_degree[b] += 1
+
+
     answer = []
     return answer
 
