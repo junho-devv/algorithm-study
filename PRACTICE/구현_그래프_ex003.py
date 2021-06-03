1d83eb6172d640fb2accf600ef7929f43c150fcd
Junho Jin <junho.devv@gmail.com>
2021-06-03 23:29:39 +0900

    Solve Algorithm Problem

--- PRACTICE/구현_그래프_ex003.py
+++ PRACTICE/구현_그래프_ex003.py
@@ -76,6 +76,7 @@ def rearrange(a_num, a_rank):
 def solution():
     # 테스트 케이스의 개수(num_tc) 입력하기
     num_tc = int(input())
+
     for tc in range(num_tc):
         # 팀의 수(num_t) 입력하기
         num_t = int(input())
