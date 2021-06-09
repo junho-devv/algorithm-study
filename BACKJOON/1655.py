8e61ee1fe938e370dcef28decc46f4b5f1f4ab82
Junho Jin <junho.devv@gmail.com>
2021-06-09 23:59:54 +0900

    Solve Algorithm Problem

--- BACKJOON/1655.py
+++ BACKJOON/1655.py
@@ -1,1 +1,17 @@
 # title : 가운데를 말해요.
+num_x = int(input())
+seq_x = []
+
+
+def solution():
+
+    for _ in range(num_x):
+        seq_x.append(int(input()))
+        seq_x.sort()
+
+        len_x = len(seq_x) - 1
+        mid_x = len_x // 2
+        print(seq_x[mid_x])
+
+
+solution()
