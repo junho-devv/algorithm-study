e8fc9ed207d8f93a73b9e7fe649aa3f6f0cce8a6
Junho <junho.devv@gmail.com>
2021-06-13 00:22:04 +0900

    Study Alogorithm

--- BACKJOON/9251.py
+++ BACKJOON/9251.py
@@ -13,14 +13,29 @@ def solution():
 
     for a in range(1, len_a + 1):
         for b in range(1, len_b + 1):
-            if str_a[a - 1] == str_b[b - 1]:
-                graph_x[b][a] = graph_x[b][a - 1] + 1
-            else:
-                graph_x[b][a] = graph_x[b][a - 1]
 
-    print(graph_x)
-    answer = 0
+            if str_a[a - 1] == str_b[b - 1]:
+                if graph_x[b][a - 1] > graph_x[b - 1][a]:
+                    graph_x[b][a] = graph_x[b][a - 1]
+                else:
+                    graph_x[b][a] = graph_x[b - 1][a] + 1
+
+            else:
+                if graph_x[b][a - 1] > graph_x[b - 1][a]:
+                    graph_x[b][a] = graph_x[b][a - 1]
+                else:
+                    graph_x[b][a] = graph_x[b - 1][a]
+
+    for i in range(len_b + 1):
+        print(graph_x[i])
+        print()
+
+    # answer = LCS
+    answer = graph_x[len_b][len_a]
+
     return answer
 
 
 print(solution())
+# ACAYKP
+# CAPCAK
