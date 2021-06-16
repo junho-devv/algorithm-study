8fd22188fe7eb37e13ffc539ebedaf8d6bbe4c17
Junho <junho.devv@gmail.com>
2021-06-16 21:30:12 +0900

    Study Alogorithm

--- BACKJOON/9251.py
+++ BACKJOON/9251.py
@@ -13,13 +13,13 @@ def solution():
 
     for a in range(1, len_a + 1):
         for b in range(1, len_b + 1):
-
+            # 문자열 A의 문자와 문자열 B의 문자가 같을 때,
             if str_a[a - 1] == str_b[b - 1]:
                 if graph_x[b][a - 1] > graph_x[b - 1][a]:
                     graph_x[b][a] = graph_x[b][a - 1]
                 else:
                     graph_x[b][a] = graph_x[b - 1][a] + 1
-
+            # 문자열 A의 문자와 문자열 B의 문자가 다를 때,
             else:
                 if graph_x[b][a - 1] > graph_x[b - 1][a]:
                     graph_x[b][a] = graph_x[b][a - 1]
