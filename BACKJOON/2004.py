18955e5951f3d6d4a35a8649e573de6f6a347808
Junho <junho.devv@gmail.com>
2021-06-28 23:38:08 +0900

    Study Algorithm

---
+++ BACKJOON/2004.py
@@ -0,0 +1,36 @@
+n, m = map(int, input().split())
+
+
+def count_num_of_2(a_num):
+    # a_num!에서 2의 개수(answer)
+    answer = 0
+    # a_num!에서 2의 개수(answer)를 구하기
+    while a_num != 0:
+        a_num = a_num // 2
+        answer += a_num
+
+    return answer
+
+
+def count_num_of_5(a_num):
+    # a_num!에서 5의 개수(answer)
+    answer = 0
+    # a_num!에서 5의 개수(answer)를 구하기
+    while a_num != 0:
+        a_num = a_num // 5
+        answer += a_num
+
+    return answer
+
+
+def solution():
+
+    if m == 0:
+        print(0)
+    else:
+        answer = min(count_num_of_2(n) - count_num_of_2(m) - count_num_of_2(n - m),
+                     count_num_of_5(n) - count_num_of_5(m) - count_num_of_5(n - m))
+
+
+print(solution())
+
