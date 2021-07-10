12e3eaf013819755bec63a25f2338130e6e026e6
Junho <junho.devv@gmail.com>
2021-07-10 22:40:55 +0900

    Study Algorithm

--- BACKJOON/1427.py
+++ BACKJOON/1427.py
@@ -5,18 +5,21 @@ def solution():
     n = int(sys.stdin.readline())
 
     arr_x = []
-
+    # 각 자릿수를 배열(arr_x)에 원소로 삽입하기
     while n // 10 != 0:
         arr_x.append(n % 10)
         n = n // 10
-
     arr_x.append(n)
-    arr_x.sort()
+    # 배열(arr_x)의 원소를 내림차순으로 정렬하기
+    arr_x.sort(reverse=True)
 
     answer = 0
+
     for i in range(len(arr_x)):
         answer += arr_x[i] * (10 ** i)
+    # 결과(answer) 출력하기
     print(answer)
+
     return
 
 
