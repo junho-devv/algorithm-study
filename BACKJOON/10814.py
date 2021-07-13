60ebfe4c0e14ad39787fca01b94f4b1107fd3816
Junho <junho.devv@gmail.com>
2021-07-13 23:30:01 +0900

    Study Algorithm

--- BACKJOON/10814.py
+++ BACKJOON/10814.py
@@ -5,7 +5,15 @@ for _ in range(num_m):
     age, name = input().split()
     arr_m.append([int(age), name])
 
-arr_m.sort(key=lambda x: x[0])
+arr_m.sort(key=lambda x: (x[0], x[1]))
 
 for i in range(num_m):
     print(arr_m[i][0], arr_m[i][1])
+
+"""
+4
+31 Junho
+27 Minho
+27 Gyungmin
+29 Cho
+"""
