0abb4b0c76a15a60d7a3b413f245e6e95743ca9d
Junho <junho.devv@gmail.com>
2021-07-20 21:59:19 +0900

    Study Algorithm

---
+++ BACKJOON/1152_단어의 개수.py
@@ -0,0 +1,24 @@
+import sys
+
+
+def solution():
+    # 단어의 개수(answer)
+    answer = 0
+    # 문자열(input_x) 입력받기
+    input_x = sys.stdin.readline()
+
+    count_x = False
+
+    for i in input_x:
+        if i == " ":
+            count_x = False
+        else:
+            if not count_x:
+                answer += 1
+                count_x = True
+
+    print(answer)
+    return
+
+
+solution()
