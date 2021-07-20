02afe816ab410fb3019f0f54078323f536dd316c
Junho <junho.devv@gmail.com>
2021-07-20 22:59:14 +0900

    Study Algorithm

--- BACKJOON/2908_상수.py
+++ BACKJOON/2908_상수.py
@@ -9,4 +9,19 @@ def solution():
     return
 
 
-solution()
+def solution_2():
+
+    num_x1, num_y1 = map(str, input().split())
+    num_x2 = list(num_x1)
+    num_x2.reverse()
+    num_y2 = list(num_y1)
+    num_y2.reverse()
+
+    num_x2 = int(''.join(num_x2))
+    num_y2 = int(''.join(num_y2))
+
+    answer = num_x2 if num_x2 > num_y2 else num_y2
+    print(answer)
+
+
+solution_2()
