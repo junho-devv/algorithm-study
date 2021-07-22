29a1b8652b4f55b9ed901b891f1d628f656d1a67
Junho <junho.devv@gmail.com>
2021-07-22 23:45:31 +0900

    Study Algorithm

--- BACKJOON/5622_다이얼.py
+++ BACKJOON/5622_다이얼.py
@@ -1,11 +1,14 @@
 def solution():
     answer = 0
     input_x = input()
+
+    arr_dial = [['A', 'B', 'C'], ['D', 'E', 'F'], ['G', 'H', 'I'], ['J', 'K', 'L'],
+                ['M', 'N', 'O'], ['P', 'Q', 'R', 'S'], ['T', 'U', 'V'], ['W', 'X', 'Y', 'Z']]
+
     for i in input_x:
-        if i == "Z":
-            answer += 9 + 1
-        else:
-            answer += ((ord(i) - 65) // 3 + 2) + 1
+        for j in arr_dial:
+            if i in j:
+                answer += arr_dial.index(j) + 3
 
     print(answer)
 
