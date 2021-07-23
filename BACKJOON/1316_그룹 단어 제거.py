4bdbc64a14e391e32e106cde14177258600a3a9e
Junho <junho.devv@gmail.com>
2021-07-23 23:02:36 +0900

    Study Algorithm

--- BACKJOON/1316_그룹 단어 제거.py
+++ BACKJOON/1316_그룹 단어 제거.py
@@ -29,5 +29,19 @@ def solution():
     return
 
 
-solution()
+def solution_1():
+    N = int(input())
+    rst = N
 
+    for i in range(0, N):
+        word = input()
+        for j in range(0, len(word) - 1):
+            if word[j] == word[j + 1]:
+                pass
+            elif word[j] in word[j + 1:]:
+                rst -= 1
+                break
+    print(rst)
+
+
+solution_1()
