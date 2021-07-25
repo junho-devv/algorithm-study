e510d37214a4976f00b55286491619d580853e11
Junho <junho.devv@gmail.com>
2021-07-26 00:44:09 +0900

    Study Algorithm

--- BACKJOON/1193_분수찾기.py
+++ BACKJOON/1193_분수찾기.py
@@ -1,20 +1,47 @@
 def solution():
     # 숫자 X(input_x) 입력받기
     input_x = int(input())
-    ord_x = 1
-    end_x = 0
-    for i in range(1, input_x):
-        if ord_x >= input_x:
-            start_x = i
+
+    stack_x = 1
+
+    while True:
+        sum_x = stack_x * (stack_x + 1) // 2
+
+        if sum_x >= input_x:
+            result_x = stack_x
             break
         else:
-            ord_x += i + 1
+            stack_x += 1
 
-    if start_x % 2 == 0:
-        diff_x = ord_x - start_x
-        
+    start_x = sum_x - result_x
+    ord_x = input_x - start_x
+
+    if stack_x % 2 == 0:
+        denom_x, numer_x = result_x - ord_x + 1, ord_x
     else:
-        print()
+        denom_x, numer_x = ord_x, result_x - ord_x + 1
+
+    answer = str(numer_x) + "/" + str(denom_x)
+
+    print(answer)
 
 
-solution()
+def solution_1():
+    input_x = int(input())
+    stack_x = 1
+
+    while input_x > stack_x:
+        input_x -= stack_x
+        stack_x += 1
+
+    if stack_x % 2 == 0:
+        dividend_x = input_x
+        divisor_x = stack_x - input_x + 1
+    else:
+        dividend_x = stack_x - input_x + 1
+        divisor_x = input_x
+
+    print("{}/{}".format(dividend_x, divisor_x))
+
+
+solution_1()
