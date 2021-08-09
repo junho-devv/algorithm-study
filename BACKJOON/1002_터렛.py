0af3b21b39f9fe7a1134fc389ac017441b58356b
Junho <junho.devv@gmail.com>
2021-08-09 23:31:57 +0900

    Study Algorithm

--- BACKJOON/1002_터렛.py
+++ BACKJOON/1002_터렛.py
@@ -7,32 +7,53 @@ num_tc = int(sys.stdin.readline())
 
 for _ in range(num_tc):
     joe_x, joe_y, joe_r, back_x, back_y, back_r = map(int, sys.stdin.readline().split())
-
     # 조규현-백승환 둘 사이의 거리(diff_jb) 계산하기
-    diff_jb = sqrt((joe_x - back_x) ** 2 + (joe_y - back_y) ** 2)
+    diff_jb = ((joe_x - back_x) ** 2 + (joe_y - back_y) ** 2) ** 0.5
 
-    # 둘의 위치가 같고 둘이 계산한 상대편의 거리가 서로 같을 때,
+    arr_r = [diff_jb, joe_r, back_r]
+    arr_r.sort()
+
     if diff_jb == 0 and joe_r == back_r:
-        # 류재명이 있을 수 있는 위치의 개수가 무한대인 경우, '-1' 출력하기
         print(-1)
+    # 두 원의 접점이 없는 경우
+    elif arr_r[2] > arr_r[0] + arr_r[1]:
+        print(0)
+    # 두 원의 접점이 1개인 경우
+    elif arr_r[2] == arr_r[0] + arr_r[1]:
+        print(1)
+    # 두 원의 접점이 2개인 경우
+    else:
+        print(2)
 
-    # 두 원이 내접인 경우,
-    elif diff_jb < max(joe_r, back_r):
-        # 두 원의 접점이 두개일 때,
-        if diff_jb + min(joe_r, back_r) > max(joe_r, back_r):
-            print(2)
-        # 두 원의 접점이 하나일 때,
-        elif diff_jb == abs(joe_r - back_r):
-            print(1)
-        # 두 원의 접점이 없을 때,
-        else:
-            print(0)
-    # 두 원이 외접인 경우,
-    elif diff_jb >= max(joe_r, back_r):
-        # 두 원의 접점이 두개일 때,
-        if diff_jb < joe_r + back_r:
-            print(2)
-        elif diff_jb == joe_r + back_r:
-            print(1)
-        else:
-            print(0)
+
+# for _ in range(num_tc):
+#     joe_x, joe_y, joe_r, back_x, back_y, back_r = map(int, sys.stdin.readline().split())
+#
+#     # 조규현-백승환 둘 사이의 거리(diff_jb) 계산하기
+#     diff_jb = sqrt((joe_x - back_x) ** 2 + (joe_y - back_y) ** 2)
+#
+#     # 둘의 위치가 같고 둘이 계산한 상대편의 거리가 서로 같을 때,
+#     if diff_jb == 0 and joe_r == back_r:
+#         # 류재명이 있을 수 있는 위치의 개수가 무한대인 경우, '-1' 출력하기
+#         print(-1)
+#
+#     # 두 원이 내접인 경우,
+#     elif diff_jb < max(joe_r, back_r):
+#         # 두 원의 접점이 두개일 때,
+#         if diff_jb + min(joe_r, back_r) > max(joe_r, back_r):
+#             print(2)
+#         # 두 원의 접점이 하나일 때,
+#         elif diff_jb == abs(joe_r - back_r):
+#             print(1)
+#         # 두 원의 접점이 없을 때,
+#         else:
+#             print(0)
+#     # 두 원이 외접인 경우,
+#     elif diff_jb >= max(joe_r, back_r):
+#         # 두 원의 접점이 두개일 때,
+#         if diff_jb < joe_r + back_r:
+#             print(2)
+#         elif diff_jb == joe_r + back_r:
+#             print(1)
+#         else:
+#             print(0)
