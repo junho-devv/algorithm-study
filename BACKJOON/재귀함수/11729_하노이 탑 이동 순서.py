ad0ab4450e8de833a0fdeff8e5ed5d0c542bc394
Junho <junho.devv@gmail.com>
2021-08-12 18:47:37 +0900

    Study Algorithm

---
+++ BACKJOON/재귀함수/11729_하노이 탑 이동 순서.py
@@ -0,0 +1,19 @@
+def move_the_hanoi_tower(x_disk, a_pole, b_pole, c_pole):
+
+    if x_disk == 1:
+        print(a_pole, c_pole)
+
+    else:
+        move_the_hanoi_tower(x_disk - 1, a_pole, c_pole, b_pole)
+        print(a_pole, c_pole)
+        move_the_hanoi_tower(x_disk - 1, b_pole, a_pole, c_pole)
+
+
+def solution():
+    # 원판의 개수(dist_x) 입력받기
+    disk_x = int(input())
+
+    move_the_hanoi_tower(disk_x, 1, 2, 3)
+
+
+solution()
