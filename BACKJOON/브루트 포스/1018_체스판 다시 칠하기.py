e7919345e9821131a1e5ed413ff25526a0297115
Junho <junho.devv@gmail.com>
2021-08-16 02:51:17 +0900

    Study Algorithm

---
+++ BACKJOON/브루트 포스/1018_체스판 다시 칠하기.py
@@ -0,0 +1,35 @@
+def solution():
+
+    size_n, size_m = map(int, input().split())
+
+    board_nm = []
+
+    for _ in range(size_n):
+        board_nm.append(str(input()))
+
+    black_board = ['BWBWBWBW', 'WBWBWBWB', 'BWBWBWBW', 'WBWBWBWB', 'BWBWBWBW', 'WBWBWBWB', 'BWBWBWBW', 'WBWBWBWB']
+    white_board = ['WBWBWBWB', 'BWBWBWBW', 'WBWBWBWB', 'BWBWBWBW', 'WBWBWBWB', 'BWBWBWBW', 'WBWBWBWB', 'BWBWBWBW']
+
+    min_nm = int(1e9)
+
+    for a in range(0, size_n - 8 + 1):
+        for b in range(0, size_m - 8 + 1):
+
+            count_b = 0
+            count_w = 0
+
+            for c in range(0, 8):
+                for d in range(0, 8):
+
+                    if black_board[c][d] != board_nm[a + c][b + d]:
+                        count_b += 1
+
+                    if white_board[c][d] != board_nm[a + c][b + d]:
+                        count_w += 1
+
+            min_nm = min(min_nm, min(count_w, count_b))
+
+    print(min_nm)
+
+
+solution()
