9614663f499bbc364ee9ced2a5b298258c6abe1a
Junho <junho.devv@gmail.com>
2021-08-22 01:46:57 +0900

    Study Algorithm

--- BACKJOON/단계별로 풀어보기/14_백트랙킹/15649_N과 M (1).py
+++ BACKJOON/단계별로 풀어보기/14_백트랙킹/15649_N과 M (1).py
@@ -2,17 +2,41 @@ from itertools import permutations
 
 
 def solution():
+    # end_x, num_x = map(int, input().split())
+    #
+    # list_x = [x for x in range(1, end_x + 1)]
+    #
+    # list_y = list(permutations(list_x, num_x))
+    #
+    # for y in list_y:
+    #     for i in y:
+    #         print(i, end=' ')
+    #     print()
 
-    end_x, num_x = map(int, input().split())
-
-    list_x = [x for x in range(1, end_x + 1)]
-
-    list_y = list(permutations(list_x, num_x))
-
-    for y in list_y:
-        for i in y:
-            print(i, end=' ')
-        print()
+    input_n, input_m = map(int, input().split())
 
 
-solution()
+def solution_2():
+    input_n, input_m = map(int, input().split())
+    list_visited = [False] * input_n
+    out = []
+
+    def solve(para_depth, para_n, para_m):
+
+        if para_depth == para_m:
+            print(' '.join(map(str, out)))
+            return
+
+        for i in range(input_n):
+
+            if not list_visited[i]:
+                list_visited[i] = True
+                out.append(i + 1)
+                solve(para_depth + 1, input_n, input_m)
+                list_visited[i] = False
+                out.pop()
+
+    solve(0, input_n, input_m)
+
+
+solution_2()
