9169d8990dc8e701dcd8b0e0bf6987d82752f3f6
Junho <junho.devv@gmail.com>
2021-08-23 01:58:59 +0900

    Study Algorithm

---
+++ BACKJOON/단계별로 풀어보기/14_백트랙킹/15650_N과 M (2).py
@@ -0,0 +1,26 @@
+def solution():
+
+    input_n, input_m = map(int, input().split())
+
+    list_visited = [False] * (input_n + 1)
+
+    list_out = []
+
+    def search(para_depth, para_n, para_m, para_max):
+        if para_depth == para_m:
+            print(' '.join(map(str, list_out)))
+            return
+
+        for i in range(1, para_n + 1):
+
+            if not list_visited[i] and para_max < i:
+                list_visited[i] = True
+                list_out.append(i)
+                search(para_depth + 1, para_n, para_m, i)
+                list_visited[i] = False
+                list_out.pop()
+
+    search(0, input_n, input_m, 0)
+
+
+solution()
