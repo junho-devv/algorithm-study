20c719442aa8d7945b1222dc29fabddaa880b862
Junho <junho.devv@gmail.com>
2021-08-27 00:29:31 +0900

    Study Algorithm

---
+++ BACKJOON/단계별로 풀어보기/14_백트랙킹/14888ㅣ연산자 끼워넣기.py
@@ -0,0 +1,50 @@
+num_N = int(input())
+list_N = list(map(int, input().split()))
+
+list_op = list(map(int, input().split()))
+temp = []
+for i in range(4):
+
+    for op in range(list_op[i]):
+        temp.append(i)
+
+temp_visited = [False] * len(temp)
+
+max_n, min_n = -int(1e9), int(1e9)
+
+
+def track(para_n, para_result):
+    global max_n, min_n
+
+    if para_n == num_N - 1:
+
+        max_n = max(max_n, para_result)
+        min_n = min(min_n, para_result)
+
+    else:
+        for x in range(len(temp)):
+            if not temp_visited[x]:
+                temp_visited[x] = True
+                temp_result = para_result
+                if temp[x] == 0:
+                    para_result += list_N[para_n + 1]
+                elif temp[x] == 1:
+                    para_result -= list_N[para_n + 1]
+                elif temp[x] == 2:
+                    para_result *= list_N[para_n + 1]
+                else:
+                    if para_result < 0:
+                        para_result = (-1) * para_result // list_N[para_n + 1]
+                        para_result *= -1
+                    else:
+                        para_result //= list_N[para_n + 1]
+
+                track(para_n + 1, para_result)
+                para_result = temp_result
+                temp_visited[x] = False
+
+
+track(0, list_N[0])
+
+print(max_n)
+print(min_n)
