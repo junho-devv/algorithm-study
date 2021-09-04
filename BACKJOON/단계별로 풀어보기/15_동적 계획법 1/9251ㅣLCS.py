6222e2eece2b84039b740ffbc7d6370b5ed0d2ae
Junho <junho.devv@gmail.com>
2021-09-05 02:18:19 +0900

    Study Algorithm

---
+++ BACKJOON/단계별로 풀어보기/15_동적 계획법 1/9251ㅣLCS.py
@@ -0,0 +1,18 @@
+str_a = input()
+str_b = input()
+
+table_dp = [[0] * (int(1e3) + 1) for _ in range(int(1e3) + 1)]
+
+for a in range(len(str_a)):
+    for b in range(len(str_b)):
+
+        if str_a[a] == str_b[b]:
+            table_dp[a + 1][b + 1] = table_dp[a][b] + 1
+
+        else:
+            table_dp[a + 1][b + 1] = max(table_dp[a + 1][b], table_dp[a][b + 1])
+
+# for i in range(len(str_a) + 1):
+#     print(table_dp[i][:len(str_b) + 1])
+
+print(table_dp[len(str_a)][len(str_b)])
