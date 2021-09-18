2e4d021df0fe394a115730e04ee18e6969da1174
Junho <junho.devv@gmail.com>
2021-09-18 23:46:40 +0900

    Study Algorithm

--- BACKJOON/단계별로 풀어보기/18_스택 단계/1874ㅣ스택 수열.py
+++ BACKJOON/단계별로 풀어보기/18_스택 단계/1874ㅣ스택 수열.py
@@ -1,40 +1,59 @@
 in_n = int(input())
 
-seq_n = []
-for _ in range(in_n):
-    seq_n.append(int(input()))
-
-stack_n = [0]
+stack_n = []
 stack_pp = []
-visited = [False] * (in_n + 1)
-
-now = 0
+now = 1
 answer = True
-for n in seq_n:
+for _ in range(in_n):
 
-    if now < n:
-        for i in range(now + 1, n + 1):
-            if not visited[i]:
-                stack_n.append(i)
-                stack_pp.append('+')
+    int_n = int(input())
 
-        visited[n] = True
+    while now <= int_n:
+        stack_n.append(now)
+        stack_pp.append('+')
+        now += 1
+
+    if stack_n[-1] == int_n:
         stack_n.pop()
         stack_pp.append('-')
-
-        now = stack_n[-1]
-
-    elif now == n:
-        visited[n] = True
-        stack_n.pop()
-        stack_pp.append('-')
-
-        now = stack_n[-1]
-
     else:
         answer = False
-        break
 
+# seq_n = []
+# for _ in range(in_n):
+#     seq_n.append(int(input()))
+#
+# stack_n = [0]
+# stack_pp = []
+# visited = [False] * (in_n + 1)
+#
+# now = 0
+# answer = True
+# for n in seq_n:
+#
+#     if now < n:
+#         for i in range(now + 1, n + 1):
+#             if not visited[i]:
+#                 stack_n.append(i)
+#                 stack_pp.append('+')
+#
+#         visited[n] = True
+#         stack_n.pop()
+#         stack_pp.append('-')
+#
+#         now = stack_n[-1]
+#
+#     elif now == n:
+#         visited[n] = True
+#         stack_n.pop()
+#         stack_pp.append('-')
+#
+#         now = stack_n[-1]
+#
+#     else:
+#         answer = False
+#         break
+#
 if answer:
     for pp in stack_pp:
         print(pp)
