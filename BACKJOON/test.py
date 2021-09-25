41c0b7554ca1a88c85bf855f88b35d610befa683
Junho <junho.devv@gmail.com>
2021-09-25 21:37:40 +0900

    Study Algorithm

--- BACKJOON/test.py
+++ BACKJOON/test.py
@@ -1,10 +1,13 @@
-import sys
-from collections import deque
+list_a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-
-input = sys.stdin.readline
-
-que_n = deque(input()[1: -2].split(','))
-que_2n = deque()
-print(que_n)
-print(que_2n)
+list_b = list_a[::2]
+print(list_b)
+list_c = list_a[1::2]
+print(list_c)
+list_d = list_a[::-2]
+print(list_d)
+list_f = list_a[1:5:2]
+print(list_f)
+list_f[0] = -1
+print(list_f)
+print(list_a)
---
+++ BACKJOON/단계별로 풀어보기/20_분할 정복/2630ㅣ색종이 만들기.py
@@ -0,0 +1,30 @@
+in_n = int(input())
+paper_n = [list(map(int, input().split())) for _ in range(in_n)]
+answer = [0] * 2
+
+
+def cut_paper(para_n, para_x, para_y):
+    # 종이 판별 과정
+    breaker = False
+    color_wb = paper_n[para_x][para_y]
+    for x in range(para_x, para_x + para_n):
+        for y in range(para_y, para_y + para_n):
+            if paper_n[x][y] != color_wb:
+                breaker = True
+                break
+        if breaker:
+            break
+
+    if breaker:
+        cut_paper(para_n // 2, para_x, para_y)
+        cut_paper(para_n // 2, para_x + para_n // 2, para_y)
+        cut_paper(para_n // 2, para_x, para_y + para_n // 2)
+        cut_paper(para_n // 2, para_x + para_n // 2, para_y + para_n // 2)
+
+    else:
+        answer[color_wb] += 1
+
+
+cut_paper(in_n, 0, 0)
+for a in answer:
+    print(a)
