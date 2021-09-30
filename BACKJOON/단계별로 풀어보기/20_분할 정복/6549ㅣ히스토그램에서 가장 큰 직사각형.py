eb4fcc5a41fe3e153a4dac776ef4c26a4e7206f1
Junho <junho.devv@gmail.com>
2021-10-01 01:55:32 +0900

    Study Algorithm

--- BACKJOON/단계별로 풀어보기/20_분할 정복/6549ㅣ히스토그램에서 가장 큰 직사각형.py
+++ BACKJOON/단계별로 풀어보기/20_분할 정복/6549ㅣ히스토그램에서 가장 큰 직사각형.py
@@ -3,8 +3,22 @@ import sys
 input = sys.stdin.readline
 
 while True:
-    histogram_n = list(map(int, input().split()))
-
-    if histogram_n[0] == 0:
+    in_n, *histogram_n = list(map(int, input().split()))
+    histogram_n.append(0)
+    if in_n == 0:
         break
 
+    answer = 0
+    stack_n = []
+
+    for n, height in enumerate(histogram_n):
+
+        while stack_n and stack_n[-1][1] > height:
+            temp_height = stack_n.pop()[1]
+            temp_width = n - (stack_n[-1][0] + 1) if stack_n else n
+
+            answer = max(answer, temp_height * temp_width)
+
+        stack_n.append([n, height])
+
+    print(answer)
