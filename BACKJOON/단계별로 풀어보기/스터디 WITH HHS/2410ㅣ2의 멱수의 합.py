c2cca76ead23aaae3c0d5cebde5267ff59f3b81c
JIN <junho.devv@gmail.com>
2021-10-15 01:13:15 +0900

    STUDY ALGORITHM

--- BACKJOON/단계별로 풀어보기/스터디 WITH HHS/2410ㅣ2의 멱수의 합.py
+++ BACKJOON/단계별로 풀어보기/스터디 WITH HHS/2410ㅣ2의 멱수의 합.py
@@ -6,9 +6,17 @@ in_n = int(sys.stdin.readline())
 table_dp = [0] * (int(1e6) + 1)
 table_dp[0] = 1
 
-for i in range(in_n):
-    if 2 ** i <= in_n:
-        for j in range(2 ** i, in_n + 1):
-            table_dp[j] += table_dp[j - 2 ** i]
+bin_n = list(bin(in_n))[2:]
+for i in range(len(bin_n)):
+    i = 2 ** i
+    for j in range(i, in_n + 1):
+        table_dp[j] += table_dp[j - i]
 
-    print(table_dp[:10])
+
+print(table_dp[in_n])
+
+# table_dp[n] = table_dp[n] + table_dp[n - 2 ** i]
+# DP 테이블에 자기자신과 자기자신에서 2 ** i를 뺀 위치의 DP 테이블 값을 합친다
+# 오름차순
+# 3의 경우에
+# 기존 1+1+1과 1에 2추가 한경우를 합하여 2
