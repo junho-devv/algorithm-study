85c2ba9221b39a679572cae585e3a10cd6f95506
JIN <junho.devv@gmail.com>
2021-10-22 19:17:21 +0900

    STUDY ALGORITHM

--- BACKJOON/단계별로 풀어보기/스터디 WITH HHS/2624ㅣ동전 바꿔주기.py
+++ BACKJOON/단계별로 풀어보기/스터디 WITH HHS/2624ㅣ동전 바꿔주기.py
@@ -6,9 +6,12 @@ in_k = int(sys.stdin.readline())
 list_k = [list(map(int, sys.stdin.readline().split())) for _ in range(in_k)]
 
 table_dp = [0 for _ in range(in_t + 1)]
+table_dp[0] = 1
 
 for k in range(in_k):
+    for t in reversed(range(1, in_t + 1)):
+        for num in range(1, list_k[k][1] + 1):
+            if 0 <= t - list_k[k][0] * num:
+                table_dp[t] += table_dp[t - list_k[k][0] * num]
 
-    for num in range(1, list_k[k][1] + 1):
-        if num - list_k[k][0] >= 0:
-            table_dp[num] += table_dp[num - list_k[k][0]]
+print(table_dp[in_t])
