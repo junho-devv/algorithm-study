0e8d73b5bc1d13dd1ed5166ffa32d03a80c0ae2f
JIN <junho.devv@gmail.com>
2021-10-27 01:40:16 +0900

    STUDY ALGORITHM

--- BACKJOON/단계별로 풀어보기/23_동적 계획법 2/11049ㅣ행렬 곱셈 순서.py
+++ BACKJOON/단계별로 풀어보기/23_동적 계획법 2/11049ㅣ행렬 곱셈 순서.py
@@ -6,10 +6,13 @@ list_matrix = [0] + [list(map(int, sys.stdin.readline().split())) for _ in range
 
 table_dp = [[0 for _ in range(in_n + 1)] for _ in range(in_n + 1)]
 
-for l in range(2, in_n + 1):
-    for i in range(1, in_n + 1):
-        table_dp[i][i + l - 1] = 2 ** 32
+for n in range(2, in_n + 1):
+    for i in range(1, in_n - n + 2):
+        table_dp[i][i + n - 1] = 2 ** 32
 
-        for k in range(i, i + l - 1):
-            table_dp[i][i + l - 1] = min(table_dp[i][i + l - 1], table_dp[i][k] + table_dp[k + 1][i + l - 1] +
-                                         list_matrix[i][0] * list_matrix[k][1] * )
+        for k in range(i, i + n - 1):
+            table_dp[i][i + n - 1] = min(table_dp[i][i + n - 1], table_dp[i][k] + table_dp[k + 1][i + n - 1] +
+                                         list_matrix[i][0] * list_matrix[k][1] * list_matrix[i + n - 1][1])
+
+for dp in table_dp:
+    print(*dp)
