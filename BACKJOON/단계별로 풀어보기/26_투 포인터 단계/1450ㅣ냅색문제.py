ea0f9b1ab73a42069160a2e07956b85d3c718407
JIN <junho.devv@gmail.com>
2021-11-12 22:03:32 +0900

    STUDY ALGORITHM

---
+++ BACKJOON/단계별로 풀어보기/26_투 포인터 단계/1450ㅣ냅색문제.py
@@ -0,0 +1,17 @@
+import sys
+
+
+def brute_force(para_list1, para_list2, para_len):
+    if len(para_list1) <= para_len:
+        return
+
+
+def binary_search():
+
+    return
+
+
+if __name__ == '__main__':
+    in_n, in_c = map(int, sys.stdin.readline().split())
+    stuff_n = list(map(int, sys.stdin.readline().split()))
+
