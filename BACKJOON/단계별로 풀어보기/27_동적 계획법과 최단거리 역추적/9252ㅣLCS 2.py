f8d40bcfcd57bd3e9a3eb4aaf081f0caebe23c52
JIN <junho.devv@gmail.com>
2021-11-19 00:25:24 +0900

    STUDY ALGORITHM

--- BACKJOON/단계별로 풀어보기/27_동적 계획법과 최단거리 역추적/9252ㅣLCS 2.py
+++ BACKJOON/단계별로 풀어보기/27_동적 계획법과 최단거리 역추적/9252ㅣLCS 2.py
@@ -11,23 +11,23 @@ def solution():
     for x in range(1, len_a + 1):
         for y in range(1, len_b + 1):
             if in_a[x] == in_b[y]:
-                if dp_ab[x - 1][y] + 1 > dp_ab[x][y - 1]:
-                    dp_ab[x][y] = max(dp_ab[x - 1][y] + 1, dp_ab[x][y - 1])
-                    dp_ab2[x][y] = dp_ab2[x - 1][y] + in_a[x]
+                if dp_ab[x - 1][y - 1] + 1 > dp_ab[x][y - 1]:
+                    dp_ab[x][y] = dp_ab[x - 1][y - 1] + 1
+                    dp_ab2[x][y] = dp_ab2[x - 1][y - 1] + in_a[x]
                 else:
                     dp_ab[x][y] = dp_ab[x][y - 1]
                     dp_ab2[x][y] = dp_ab2[x][y - 1]
             else:
                 if dp_ab[x - 1][y] > dp_ab[x][y - 1]:
-                    dp_ab[x][y] = max(dp_ab[x - 1][y], dp_ab[x][y - 1])
+                    dp_ab[x][y] = dp_ab[x - 1][y]
                     dp_ab2[x][y] = dp_ab2[x - 1][y]
                 else:
                     dp_ab[x][y] = dp_ab[x][y - 1]
                     dp_ab2[x][y] = dp_ab2[x][y - 1]
-    for ab in dp_ab:
-        print(*ab)
-    for ab in dp_ab2:
-        print(*ab)
+    # for ab in dp_ab:
+    #     print(*ab)
+    # for ab in dp_ab2:
+    #     print(*ab)
 
     print(dp_ab[len_a][len_b])
     print(dp_ab2[len_a][len_b])
