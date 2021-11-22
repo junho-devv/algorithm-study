27fd6b5363c02167f8992be229618597248ebd95
JIN <junho.devv@gmail.com>
2021-11-22 23:54:56 +0900

    STUDY ALGORITHM

--- BACKJOON/단계별로 풀어보기/27_동적 계획법과 최단거리 역추적/13913ㅣ숨바꼭질 4.py
+++ BACKJOON/단계별로 풀어보기/27_동적 계획법과 최단거리 역추적/13913ㅣ숨바꼭질 4.py
@@ -2,7 +2,22 @@ from collections import deque
 import sys
 
 
+def seek_path(para_pos):
+    answer = []
+    temp_pos = para_pos
+
+    for _ in range(position_seek[para_pos]):
+        answer.append(temp_pos)
+        temp_pos = path_seek[temp_pos]
+
+    answer.reverse()
+
+    return answer
+
+
 def seek():
+    que_seek = deque([in_n])
+
     while que_seek:
         temp_pos = que_seek.popleft()
         if temp_pos == in_k:
@@ -14,10 +29,7 @@ def seek():
             if 0 <= next_pos < int(1e5) + 1:
                 if position_seek[next_pos] == 0:
                     position_seek[next_pos] = position_seek[temp_pos] + 1
-
-                    path_seek[next_pos] = path_seek[temp_pos][:]
-                    path_seek[next_pos].append(next_pos)
-
+                    path_seek[next_pos] = temp_pos
                     que_seek.append(next_pos)
 
 
@@ -27,12 +39,12 @@ if __name__ == '__main__':
     position_seek = [0] * (int(1e5) + 1)
     position_seek[in_n] = 1
 
-    path_seek = [[] for _ in range(int(1e5) + 1)]
-    path_seek[in_n].append(in_n)
-
-    que_seek = deque([in_n])
+    path_seek = [0] * (int(1e5) + 1)
+    path_seek[in_n] = -1
 
     seek()
 
-    print(position_seek[in_k] - 1)
-    print(*path_seek[in_k])
+    out_1 = position_seek[in_k] - 1
+    out_2 = seek_path(in_k)
+    print(out_1)
+    print(*out_2)
