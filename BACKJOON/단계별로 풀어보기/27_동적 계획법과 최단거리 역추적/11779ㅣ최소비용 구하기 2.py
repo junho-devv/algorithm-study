a50c9fa9a04b7aa99d7b7f3f767463bb2176e49b
JIN <junho.devv@gmail.com>
2021-11-25 23:50:18 +0900

    STUDY ALGORITHM

---
+++ BACKJOON/단계별로 풀어보기/27_동적 계획법과 최단거리 역추적/11779ㅣ최소비용 구하기 2.py
@@ -0,0 +1,11 @@
+import sys
+
+
+if __name__ == '__main__':
+    in_n = int(sys.stdin.readline())
+    in_m = int(sys.stdin.readline())
+
+    for _ in range(in_m):
+        in_a, in_b, in_c = map(int, sys.stdin.readline().split())
+
+    in_s, in_d = map(int, sys.stdin.readline().split())
