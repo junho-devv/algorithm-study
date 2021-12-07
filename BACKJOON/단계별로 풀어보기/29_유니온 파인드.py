fe131cb84f50e3903e08221ade94d712aab26306
JIN <junho.devv@gmail.com>
2021-12-08 01:14:55 +0900

    STUDY ALGORITHM

--- BACKJOON/단계별로 풀어보기/29_유니온 파인드.py
+++ BACKJOON/단계별로 풀어보기/29_유니온 파인드.py
@@ -1,38 +1,36 @@
 import sys
 
+sys.setrecursionlimit(10 ** 6)
+
 
 def find_parent_node(para_node):
-    answer = para_node
-    if set_n[para_node] != para_node:
-        answer = find_parent_node(set_n[para_node])
+    if para_node == set_n[para_node]:
+        return para_node
+    root_node = find_parent_node(set_n[para_node])
+    set_n[para_node] = root_node
+    return set_n[para_node]
 
-    return answer
 
-
-def find(para_a, para_b):
+def union(para_a, para_b):
     root_a, root_b = find_parent_node(para_a), find_parent_node(para_b)
-    if root_a == root_b:
-        return True
+    if root_a > root_b:
+        set_n[root_a] = root_b
     else:
-        return False
+        set_n[root_b] = root_a
 
 
 if __name__ == '__main__':
     in_n, in_m = map(int, sys.stdin.readline().split())
 
-    set_n = [n for n in range(10 ** 6 + 1)]
+    set_n = [n for n in range(in_n + 1)]
     for _ in range(in_m):
         in_cmd, in_a, in_b = map(int, sys.stdin.readline().split())
 
         if in_cmd == 0:
-            if in_a > in_b:
-                in_a, in_b = in_b, in_a
-
-            set_n[in_b] = in_a
+            union(in_a, in_b)
 
         else:
-            out_same = find(in_a, in_b)
-            if out_same:
+            if find_parent_node(in_a) == find_parent_node(in_b):
                 print("YES")
             else:
                 print("NO")
