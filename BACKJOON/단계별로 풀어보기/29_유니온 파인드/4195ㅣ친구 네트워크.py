ddf6a7151095a0e67b68922315f1fde4f6b5b1bc
JIN <junho.devv@gmail.com>
2021-12-11 23:30:08 +0900

    STUDY ALGORITHM

--- BACKJOON/단계별로 풀어보기/29_유니온 파인드/4195ㅣ친구 네트워크.py
+++ BACKJOON/단계별로 풀어보기/29_유니온 파인드/4195ㅣ친구 네트워크.py
@@ -2,21 +2,36 @@ import sys
 
 
 def find(para_node):
-    if parent[para_node] == para_node:
+    if parent[para_node] != para_node:
         parent[para_node] = find(parent[para_node])
 
     return parent[para_node]
 
 
 def union(para_a, para_b):
-
-    return
+    root_a, root_b = find(para_a), find(para_b)
+    if root_a != root_b:
+        parent[root_b] = root_a
+        friend[root_a] += friend[root_b]
 
 
 if __name__ == '__main__':
     in_tc = int(sys.stdin.readline())
     for _ in range(in_tc):
         in_f = int(sys.stdin.readline())
+
+        parent = dict()
+        friend = dict()
         for _ in range(in_f):
-            in_a, in_b = map(int, sys.stdin.readline().split())
-            if
+            in_a, in_b = map(str, sys.stdin.readline().split())
+            if in_a not in parent:
+                parent[in_a] = in_a
+                friend[in_a] = 1
+            if in_b not in parent:
+                parent[in_b] = in_b
+                friend[in_b] = 1
+
+            union(in_a, in_b)
+
+            out_friends = friend[find(in_a)]
+            print(out_friends)
