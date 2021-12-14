201926ff520c029b53507c2e49325188da7e6b8d
JIN <junho.devv@gmail.com>
2021-12-15 00:20:09 +0900

    STUDY ALGORITHM

--- BACKJOON/단계별로 풀어보기/30_최소 신장 트리/1197ㅣ최소 스패닝 트리.py
+++ BACKJOON/단계별로 풀어보기/30_최소 신장 트리/1197ㅣ최소 스패닝 트리.py
@@ -1,10 +1,37 @@
 import sys
 
 
+def find(para_node):
+    if parent_node[para_node] != para_node:
+        parent_node[para_node] = find(parent_node[para_node])
+
+    return parent_node[para_node]
+
+
+def union(para_n1, para_n2):
+    root_n1, root_n2 = find(para_n1), find(para_n2)
+    if root_n1 > root_n2:
+        root_n1, root_n2 = root_n2, root_n1
+
+    parent_node[root_n2] = root_n1
+
+
 if __name__ == '__main__':
+    # 크루스칼(Kruskal) 알고리즘을 이용한 풀이
     in_v, in_e = map(int, sys.stdin.readline().split())
 
-    tree_n = [[] for _ in range(in_v + 1)]
+    edge_e = []
     for _ in range(in_e):
         in_a, in_b, in_c = map(int, sys.stdin.readline().split())
-        tree_n.append()
+        edge_e.append([in_a, in_b, in_c])
+    edge_e.sort(key=lambda e: e[2])
+
+    parent_node = [n for n in range(in_v + 1)]
+    out_min = 0
+    for a, b, c in edge_e:
+        root_a, root_b = find(a), find(b)
+        if root_a != root_b:
+            union(a, b)
+            out_min += c
+
+    print(out_min)
