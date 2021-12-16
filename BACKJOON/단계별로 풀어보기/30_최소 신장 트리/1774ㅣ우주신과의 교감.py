d2f601ad0f4911dcfa693a1251292e1a9df997a6
JIN <junho.devv@gmail.com>
2021-12-17 00:37:12 +0900

    STUDY ALGORITHM

--- BACKJOON/단계별로 풀어보기/30_최소 신장 트리/1774ㅣ우주신과의 교감.py
+++ BACKJOON/단계별로 풀어보기/30_최소 신장 트리/1774ㅣ우주신과의 교감.py
@@ -24,17 +24,24 @@ if __name__ == '__main__':
         in_a, in_b = map(int, sys.stdin.readline().split())
         god_n.append((in_a, in_b))
 
-    dist_n = []
-    for _1 in range(in_n):
-        for _2 in range(_1 + 1, in_n):
-            temp_1, temp_2 = god_n[_1], god_n[_2]
-            if find(_1 + 1) != find(_2 + 1):
-                temp_dist = ((temp_1[0] - temp_2[0]) ** 2 + (temp_1[1] - temp_2[1]) ** 2) ** 0.5
-                dist_n.append([temp_1, temp_2, temp_dist])
-    dist_n.sort(key=lambda _: _[2])
-
-    parent_node = [_ for _ in range(in_n + 1)]
+    parent_node = [_ for _ in range(in_n)]
+    out_min = 0
     for _ in range(in_m):
         in_a, in_b = map(int, sys.stdin.readline().split())
-        union(in_a, in_b)
+        union(in_a - 1, in_b - 1)
 
+    dist_n = []
+    for _1 in range(in_n - 1):
+        for _2 in range(_1 + 1, in_n):
+            temp_1, temp_2 = god_n[_1], god_n[_2]
+            if find(_1) != find(_2):
+                temp_dist = ((temp_1[0] - temp_2[0]) ** 2 + (temp_1[1] - temp_2[1]) ** 2) ** 0.5
+                dist_n.append([_1, _2, temp_dist])
+    dist_n.sort(key=lambda _: _[2])
+
+    for node_1, node_2, cost in dist_n:
+        if find(node_1) != find(node_2):
+            union(node_1, node_2)
+            out_min += cost
+
+    print("%0.2f" % out_min)
