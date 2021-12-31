99401065d069ea398914e4cc284f6d2dacbf9f49
JIN <junho.devv@gmail.com>
2022-01-01 00:01:57 +0900

    STUDY ALGORITHM

--- BACKJOON/단계별로 풀어보기/31_트리에서의 동적 계획법/15681ㅣ트리와 쿼리.py
+++ BACKJOON/단계별로 풀어보기/31_트리에서의 동적 계획법/15681ㅣ트리와 쿼리.py
@@ -1,13 +1,17 @@
 import sys
 
 
-def count(para_):
-    list_cnt[para_] = 1
-
-    return
+def count(para_root):
+    list_cnt[para_root] = 1
+    for p_node in tree_n[para_root]:
+        if list_cnt[p_node] == 0:
+            count(p_node)
+            list_cnt[para_root] += list_cnt[p_node]
 
 
 if __name__ == '__main__':
+    sys.setrecursionlimit(10 ** 9)
+
     in_n, in_r, in_q = map(int, sys.stdin.readline().split())
     tree_n = [[] for _ in range(in_n + 1)]
     list_cnt = [0] * (in_n + 1)
@@ -17,3 +21,8 @@ if __name__ == '__main__':
         tree_n[in_a].append(in_b)
         tree_n[in_b].append(in_a)
 
+    count(in_r)
+
+    for _ in range(in_q):
+        in_u = int(sys.stdin.readline())
+        print(list_cnt[in_u])
