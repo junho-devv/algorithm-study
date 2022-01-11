7a51311c8a7e51db2fb3b4a693970af9ddd32598
JIN <junho.devv@gmail.com>
2022-01-11 23:46:47 +0900

    STUDY ALGORITHM

--- BACKJOON/단계별로 풀어보기/31_트리에서의 동적 계획법/2213ㅣ트리의 독립집합.py
+++ BACKJOON/단계별로 풀어보기/31_트리에서의 동적 계획법/2213ㅣ트리의 독립집합.py
@@ -3,6 +3,14 @@ import sys
 
 if __name__ == '__main__':
     in_n = int(sys.stdin.readline())
-    for _ in range(in_n):
-        in_w = map(int, sys.stdin.readline().split())
+    in_w = list(map(int, sys.stdin.readline().split()))
+
+    tree_n = [[] for _ in range(in_n + 1)]
+    for _ in range(in_n - 1):
+        in_a, in_b = map(int, sys.stdin.readline().split())
+        tree_n[in_a].append(in_b)
+        tree_n[in_b].append(in_a)
+
+
+
 
