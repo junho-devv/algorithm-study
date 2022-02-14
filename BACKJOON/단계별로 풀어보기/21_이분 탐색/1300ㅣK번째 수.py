de19eb463deea0b1a9e02faf20476006adcebdec
JIN <junho.devv@gmail.com>
2022-02-14 22:25:53 +0900

    STUDY ALGORITHM

--- BACKJOON/단계별로 풀어보기/21_이분 탐색/1300ㅣK번째 수.py
+++ BACKJOON/단계별로 풀어보기/21_이분 탐색/1300ㅣK번째 수.py
@@ -1,21 +1,22 @@
 import sys
 
-input = sys.stdin.readline
 
-in_n, in_k = int(input()), int(input())
-# k번째 값은 최소 1 * 1 보다 크고 k 보다 작거나 같다
-low_k, high_k = 1, in_k
-answer = 0
-while low_k <= high_k:
-    center_k = (low_k + high_k) // 2
-    cnt_k = 0
-    for n in range(1, in_n + 1):
-        cnt_k += min(center_k // n, in_n)
+if __name__ == '__main__':
+    in_n, in_k = int(sys.stdin.readline()), int(sys.stdin.readline())
 
-    if cnt_k >= in_k:
-        high_k = center_k - 1
-        answer = center_k
-    else:
-        low_k = center_k + 1
+    # k번째 값은 최소 1 * 1 보다 크고 k 보다 작거나 같다
+    low_k, high_k = 1, in_k
+    answer = 0
+    while low_k <= high_k:
+        center_k = (low_k + high_k) // 2
+        cnt_k = 0
+        for n in range(1, in_n + 1):
+            cnt_k += min(center_k // n, in_n)
 
-print(answer)
+        if cnt_k >= in_k:
+            high_k = center_k - 1
+            answer = center_k
+        else:
+            low_k = center_k + 1
+
+    print(answer)
