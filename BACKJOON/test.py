f49a7b91b3b2d0215731e5056baa840c39f774a9
JIN <junho.devv@gmail.com>
2022-02-19 21:59:40 +0900

    STUDY ALGORITHM

--- BACKJOON/test.py
+++ BACKJOON/test.py
@@ -2,4 +2,9 @@ import sys
 
 
 if __name__ == '__main__':
-    in_n = int(sys.stdin.readline())
+    in_n = list(map(int, sys.stdin.readline().split()))
+    print(in_n)
+    set_n = set(in_n)
+    print(set_n)
+    list_n = list(set_n)
+    print(list_n)
