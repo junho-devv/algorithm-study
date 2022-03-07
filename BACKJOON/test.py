11ce31743418ca39a81652cf8123faf3ae9d24cc
JIN <junho.devv@gmail.com>
2022-03-07 23:21:25 +0900

    STUDY ALGORITHM

--- BACKJOON/test.py
+++ BACKJOON/test.py
@@ -1,5 +1,22 @@
 import sys
 
 
+def fibonacci(para_n):
+    f_var1 = 0
+    f_var2 = 1
+
+    answer = 1
+
+    for p_ in range(1, para_n):
+        answer = f_var1 + f_var2
+        f_var1 = f_var2
+        f_var2 = answer
+
+    return answer
+
+
 if __name__ == '__main__':
-    print(1)
+    in_n = int(sys.stdin.readline())
+
+    out_ = fibonacci(in_n)
+    print(out_)
