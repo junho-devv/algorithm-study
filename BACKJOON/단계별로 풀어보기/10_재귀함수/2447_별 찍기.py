9503d46535b1c0fbebae2e567879d5c965ae6ef5
JIN <junho.devv@gmail.com>
2022-03-24 22:32:44 +0900

    STUDY ALGORITHM

--- BACKJOON/단계별로 풀어보기/10_재귀함수/2447_별 찍기.py
+++ BACKJOON/단계별로 풀어보기/10_재귀함수/2447_별 찍기.py
@@ -9,26 +9,27 @@ def draw_stars(a_size):
 
     draw_stars(a_size // 3)
 
-    for a in range(3):
-        for b in range(3):
+    for f_a in range(3):
+        for f_b in range(3):
 
-            if a == 1 and b == 1:
+            if f_a == 1 and f_b == 1:
                 continue
 
-            for c in range(a_size // 3):
-                map_x[(a_size // 3) * a + c][(a_size // 3) * b: (a_size // 3) * (b + 1)] = map_x[c][:(a_size // 3)]
+            for f_c in range(a_size // 3):
+                map_x[(a_size // 3) * f_a + f_c][(a_size // 3) * f_b: (a_size // 3) * (f_b + 1)] = map_x[f_c][:(a_size // 3)]
 
     return
 
 
-# 3 <= input_x < 3 ** 8
-input_x = int(input())
+if __name__ == '__main__':
+    # 3 <= input_x < 3 ** 8
+    input_x = int(input())
 
-map_x = [[' '] * input_x for _ in range(input_x)]
+    map_x = [[' '] * input_x for _ in range(input_x)]
 
-draw_stars(input_x)
+    draw_stars(input_x)
 
-for a in map_x:
-    for b in a:
-        print(b, end='')
-    print()
+    for a in map_x:
+        for b in a:
+            print(b, end='')
+        print()
