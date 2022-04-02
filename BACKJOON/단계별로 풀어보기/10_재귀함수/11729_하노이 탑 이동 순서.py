b6a49bcebc344f8289388749d3f092d05a8ce322
JIN <junho.devv@gmail.com>
2022-04-02 20:43:41 +0900

    STUDY ALGORITHM

--- BACKJOON/단계별로 풀어보기/10_재귀함수/11729_하노이 탑 이동 순서.py
+++ BACKJOON/단계별로 풀어보기/10_재귀함수/11729_하노이 탑 이동 순서.py
@@ -1,15 +1,15 @@
 import sys
 
 
-def move_the_hanoi_tower(para_disk, a_pole, b_pole, c_pole):
+def move_the_hanoi_tower(para_disk, para_pole_1, para_pole_2, para_pole_3):
 
     if para_disk == 1:
-        print(a_pole, c_pole)
+        print(para_pole_1, para_pole_3)
 
     else:
-        move_the_hanoi_tower(para_disk - 1, a_pole, c_pole, b_pole)
-        print(a_pole, c_pole)
-        move_the_hanoi_tower(para_disk - 1, b_pole, a_pole, c_pole)
+        move_the_hanoi_tower(para_disk - 1, para_pole_1, para_pole_3, para_pole_2)
+        print(para_pole_1, para_pole_3)
+        move_the_hanoi_tower(para_disk - 1, para_pole_2, para_pole_1, para_pole_3)
 
 
 def solution():
