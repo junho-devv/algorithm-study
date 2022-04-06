6872918c5fb001604a28d904e56272f8b72d2b96
JIN <junho.devv@gmail.com>
2022-04-06 20:45:47 +0900

    STUDY ALGORITHM

--- PROGRAMMERS/가장 큰 수.py
+++ PROGRAMMERS/가장 큰 수.py
@@ -5,15 +5,17 @@ def solution(numbers):
     answer = ''
 
     list_str = []
-    for number in numbers:
-        list_str.append(str(number)[0])
+    for number in range(0, len(numbers)):
+        list_str.append([str(numbers[number])[0], number])
 
+    print(list_str)
+    list_str.sort(reverse=True, key=lambda x: x[0])
     print(list_str)
 
     return answer
 
 
 if __name__ == '__main__':
-    in_x = map(int, sys.stdin.readline().split())
+    in_x = list(map(int, sys.stdin.readline().split()))
 
     solution(in_x)
