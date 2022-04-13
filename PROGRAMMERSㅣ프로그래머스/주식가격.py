c080362bfd78c9e62bf06fd23e3b4e34fef20b9d
JIN <junho.devv@gmail.com>
2022-04-14 00:50:35 +0900

    STUDY ALGORITHM

--- PROGRAMMERSㅣ프로그래머스/주식가격.py
+++ PROGRAMMERSㅣ프로그래머스/주식가격.py
@@ -1,6 +1,13 @@
 def solution(prices):
     answer = []
-    
+
+    for idx_1 in range(len(prices)):
+        temp_time = 0
+        for idx_2 in range(idx_1 + 1, len(prices)):
+            temp_time += 1
+            if prices[idx_1] > prices[idx_2]:
+                break
+        answer.append(temp_time)
 
     return answer
 
