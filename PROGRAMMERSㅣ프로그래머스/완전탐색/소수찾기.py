be30965314f5e04f01b80dde491919ba49ff9eb4
JIN <junho.devv@gmail.com>
2022-04-17 00:25:18 +0900

    STUDY ALGORITHM

--- PROGRAMMERSㅣ프로그래머스/완전탐색/소수찾기.py
+++ PROGRAMMERSㅣ프로그래머스/완전탐색/소수찾기.py
@@ -1,11 +1,23 @@
 def solution(numbers):
     answer = 0
 
+    from itertools import permutations
+
+    temp_set = set()
+    for _ in range(len(numbers)):
+        temp_set |= set(map(int, map(''.join, permutations(numbers, _ + 1))))
+
+    temp_set -= set([0, 1])
+
+    for _ in range(2, int(max(temp_set) ** 0.5) + 1):
+        temp_set -= set(range(_ * 2, max(temp_set) + 1, _))
+
+    answer += len(temp_set)
 
     return answer
 
 
 if __name__ == '__main__':
-    in_n = "17"
+    in_n = "011"
 
     solution(in_n)
