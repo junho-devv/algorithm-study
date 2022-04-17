01a972d3838adc4cca736f4cd5d519ff187c0731
JIN <junho.devv@gmail.com>
2022-04-18 00:06:52 +0900

    STUDY ALGORITHM

--- PROGRAMMERSㅣ프로그래머스/완전탐색/카펫.py
+++ PROGRAMMERSㅣ프로그래머스/완전탐색/카펫.py
@@ -1,9 +1,17 @@
 def solution(brown, yellow):
     answer = []
+
+    for h in range(1, int(yellow ** 0.5) + 1):
+        if yellow % h == 0:
+            w = yellow // h
+            if 2 * (h + w) == brown - 4:
+                answer.append(w + 2)
+                answer.append(h + 2)
+    print(answer)
     return answer
 
 
 if __name__ == '__main__':
-    in_b, in_y = 10, 2
+    in_b, in_y = 24, 24
 
     solution(in_b, in_y)
