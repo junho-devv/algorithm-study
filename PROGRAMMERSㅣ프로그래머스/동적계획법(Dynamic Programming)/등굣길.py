4933b02676121c369510e691e570345cfa442fb1
JIN <junho.devv@gmail.com>
2022-04-26 02:05:40 +0900

    STUDY ALGORITHM

---
+++ PROGRAMMERSㅣ프로그래머스/동적계획법(Dynamic Programming)/등굣길.py
@@ -0,0 +1,26 @@
+def solution(m, n, puddles):
+
+    dp_distance = [[0 for _ in range(m + 1)] for _ in range(n + 1)]
+    dp_distance[1][1] = 1
+
+    for y in range(1, n + 1):
+        for x in range(1, m + 1):
+            if x == 1 and y == 1:
+                continue
+
+            if [x, y] in puddles:
+                dp_distance[y][x] = 0
+            else:
+                dp_distance[y][x] = (dp_distance[y - 1][x] + dp_distance[y][x - 1]) % 1000000007
+
+    answer = dp_distance[n][m]
+
+    return answer
+
+
+if __name__ == '__main__':
+    in_m = 4
+    in_n = 2
+    in_puddles = [[2, 2]]
+
+    solution(in_m, in_n, in_puddles)
