b79d7d68933c30e1bb0d24e95da5a014337a4e6f
JIN <junho.devv@gmail.com>
2022-04-27 01:32:44 +0900

    STUDY ALGORITHM

---
+++ PROGRAMMERSㅣ프로그래머스/동적계획법(Dynamic Programming)/도둑질.py
@@ -0,0 +1,23 @@
+def solution(money):
+    # 첫번째 집을 무조건 털면, 마지막 집은 선택권이 없다.
+    dp_money_1 = [0] * len(money)
+    dp_money_1[0] = money[0]
+
+    for idx in range(1, len(money) - 1):
+        dp_money_1[idx] = max(dp_money_1[idx - 1], dp_money_1[idx - 2] + money[idx])
+    # 첫번째 집을 털지않고, 마지막 집에 선택권을 주게한다.
+    dp_money_2 = [0] * len(money)
+    dp_money_2[0] = 0
+
+    for idx in range(1, len(money)):
+        dp_money_2[idx] = max(dp_money_2[idx - 1], dp_money_2[idx - 2] + money[idx])
+
+    answer = max(max(dp_money_1), max(dp_money_2))
+
+    return answer
+
+
+if __name__ == '__main__':
+    in_m = [1, 2, 3, 1]
+
+    solution(in_m)
