15b0ce38b06b8697e4d6ab133b3734c86a7ec9e2
JIN <junho.devv@gmail.com>
2022-05-01 00:34:58 +0900

    STUDY ALGORITHM

--- PROGRAMMERSㅣ프로그래머스/DFS, BFS/네트워크.py
+++ PROGRAMMERSㅣ프로그래머스/DFS, BFS/네트워크.py
@@ -1,14 +1,25 @@
 def solution(n, computers):
 
-    def DFS(node):
+    def DFS_recursive(node):
         visited[node] = True
 
         for n_com in range(n):
             if computers[node][n_com] and not visited[n_com]:
-                DFS(n_com)
+                DFS_recursive(n_com)
 
     visited = [False] * n
     answer = 0
+
+    def DFS_stack(node):
+
+        stack_node = [node]
+        while stack_node:
+            node = stack_node.pop()
+            visited[node] = True
+
+            for n_com in range(n):
+                if computers[node][n_com] and not visited[n_com]:
+                    stack_node.append(n_com)
 
     def BFS(node):
         from collections import deque
@@ -24,7 +35,10 @@ def solution(n, computers):
 
     for com in range(n):
         if not visited[com]:
-            DFS(com)
+            # DFS_recursive(com)
+            # DFS_stack(com)
+            # BFS
+
             answer += 1
 
     return answer
