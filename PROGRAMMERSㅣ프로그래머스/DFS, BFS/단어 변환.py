2071652c948b44ca7ca8e5d1861cc3061057da52
JIN <junho.devv@gmail.com>
2022-05-02 00:03:50 +0900

    STUDY ALGORITHM

--- PROGRAMMERSㅣ프로그래머스/DFS, BFS/단어 변환.py
+++ PROGRAMMERSㅣ프로그래머스/DFS, BFS/단어 변환.py
@@ -1,30 +1,59 @@
 def solution(begin, target, words):
 
-    def DFS_stack(idx):
+    def DFS_stack(word):
+
         visited = [False] * len(words)
-        answer = 0
+        answer = int(1e9)
 
-        stack = [(idx, 0)]
+        stack = [(begin, 0)]
         while stack:
-            idx, length = stack.pop()
-            visited[idx] = True
+            word, length = stack.pop()
 
-            if words[idx] == target:
-                break
+            if word == target:
+                answer = min(answer, length)
 
-            for n_idx in range(len(words)):
+            for idx in range(len(words)):
                 temp_cnt = 0
-                for c_1, c_2 in zip(words[idx], words[n_idx]):
+                for c_1, c_2 in zip(word, words[idx]):
                     if c_1 != c_2:
                         temp_cnt += 1
 
-                if temp_cnt != 1 and not visited[idx]:
-                    stack.append((n_idx, length + 1))
-                    answer += 1
+                if temp_cnt == 1 and not visited[idx]:
+                    visited[idx] = True
+                    stack.append((words[idx], length + 1))
 
-    DFS_stack(0)
+        return answer
 
-    return answer
+    def BFS(word):
+        visited = [False] * len(words)
+        answer = 0
+
+        from collections import deque
+
+        que_word = deque([[word, 0]])
+        while que_word:
+            word, cnt = que_word.popleft()
+            if word == target:
+                answer = cnt
+                break
+
+            for idx in range(len(words)):
+                temp_cnt = 0
+                for c_1, c_2 in zip(word, words[idx]):
+                    if c_1 != c_2:
+                        temp_cnt += 1
+
+                if temp_cnt == 1 and not visited[idx]:
+                    visited[idx] = True
+                    que_word.append([words[idx], cnt + 1])
+
+        return answer
+
+    if target in words:
+        return BFS(begin)
+        # return DFS_stack(begin)
+    else:
+        return 0
 
 
 if __name__ == '__main__':
@@ -32,4 +61,4 @@ if __name__ == '__main__':
     in_t = "cog"
     in_w = ["hot", "dot", "dog", "lot", "log", "cog"]
 
-    solution(in_b, in_t, in_w)
+    print(solution(in_b, in_t, in_w))
