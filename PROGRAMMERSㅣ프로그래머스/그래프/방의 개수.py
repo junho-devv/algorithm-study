dc4db8582518aec598a1317df908610779535530
JIN <junho.devv@gmail.com>
2022-05-09 01:17:44 +0900

    STUDY ALGORITHM

--- PROGRAMMERSㅣ프로그래머스/그래프/방의 개수.py
+++ PROGRAMMERSㅣ프로그래머스/그래프/방의 개수.py
@@ -1,9 +1,44 @@
 def solution(arrows):
+
+    move_9 = [(0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1)]
+    now_xy = (0, 0)
+
+    from collections import deque, defaultdict
+
+    que_xy = deque([now_xy])
+    for arrow in arrows:
+        for _ in range(2):
+            next_xy = (now_xy[0] + move_9[arrow][0], now_xy[1] + move_9[arrow][1])
+            que_xy.append(next_xy)
+
+            now_xy = next_xy
+
+    visited_node = defaultdict(bool)
+    visited_path = defaultdict(bool)
+
     answer = 0
+
+    now_xy = que_xy.popleft()
+    visited_node[now_xy] = True
+
+    while que_xy:
+        next_xy = que_xy.popleft()
+
+        if visited_node[next_xy]:
+            if not visited_path[(now_xy, next_xy)]:
+                answer += 1
+        else:
+            visited_node[next_xy] = True
+
+        visited_path[(now_xy, next_xy)] = True
+        visited_path[(next_xy, now_xy)] = True
+
+        now_xy = next_xy
+
     return answer
 
 
 if __name__ == '__main__':
     in_a = [6, 6, 6, 4, 4, 4, 2, 2, 2, 0, 0, 0, 1, 6, 5, 5, 3, 6, 0]
 
-    solution(in_a)
+    print(solution(in_a))
