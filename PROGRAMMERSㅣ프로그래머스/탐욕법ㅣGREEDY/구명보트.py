8b588f0a4143475c917a1ff00f706ec95a712223
JIN <junho.devv@gmail.com>
2022-05-12 12:49:18 +0900

    STUDY ALGORITHM

---
+++ PROGRAMMERSㅣ프로그래머스/탐욕법ㅣGREEDY/구명보트.py
@@ -0,0 +1,24 @@
+def solution(people, limit):
+
+    people.sort(reverse=True)
+
+    idx_l, idx_r = 0, len(people) - 1
+    answer = 0
+
+    while idx_l <= idx_r:
+
+        answer += 1
+
+        if people[idx_l] + people[idx_r] <= limit:
+            idx_r -= 1
+
+        idx_l += 1
+
+    return answer
+
+
+if __name__ == '__main__':
+    in_p = [70, 50, 80, 50]
+    in_l = 100
+
+    print(solution(in_p, in_l))
