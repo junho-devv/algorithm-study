8ace85541a322a0f3ee1c24d971df7397b8a1b8c
JIN <junho.devv@gmail.com>
2022-05-25 17:19:00 +0900

    STUDY ALGORITHM

--- BACKJOONㅣ백준/단계별로 풀어보기/15_동적 계획법 1/9251ㅣLCS.py
+++ BACKJOONㅣ백준/단계별로 풀어보기/15_동적 계획법 1/9251ㅣLCS.py
@@ -1,15 +1,26 @@
-str_a = input()
-str_b = input()
+def solution(str_a, str_b):
 
-table_dp = [[0] * (int(1e3) + 1) for _ in range(int(1e3) + 1)]
+    table_dp = [[0] * (int(1e3) + 1) for _ in range(int(1e3) + 1)]
 
-for a in range(len(str_a)):
-    for b in range(len(str_b)):
+    for a in range(len(str_a)):
+        for b in range(len(str_b)):
 
-        if str_a[a] == str_b[b]:
-            table_dp[a + 1][b + 1] = table_dp[a][b] + 1
+            if str_a[a] == str_b[b]:
+                table_dp[a + 1][b + 1] = table_dp[a][b] + 1
 
-        else:
-            table_dp[a + 1][b + 1] = max(table_dp[a + 1][b], table_dp[a][b + 1])
+            else:
+                table_dp[a + 1][b + 1] = max(table_dp[a + 1][b], table_dp[a][b + 1])
 
-print(table_dp[len(str_a)][len(str_b)])
+    answer = table_dp[len(str_a)][len(str_b)]
+
+    return answer
+
+
+if __name__ == '__main__':
+
+    import sys
+
+    in_a = sys.stdin.readline()
+    in_b = sys.stdin.readline()
+
+    print(solution(in_a, in_b))
