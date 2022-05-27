0f998a8c2fe1608e25f24f7fb516c6725c26d2d2
JIN <junho.devv@gmail.com>
2022-05-27 21:52:33 +0900

    STUDY ALGORITHM

--- BACKJOONㅣ백준/단계별로 풀어보기/15_동적 계획법 1/11054ㅣ가장 긴 바이토닉 부분 수열.py
+++ BACKJOONㅣ백준/단계별로 풀어보기/15_동적 계획법 1/11054ㅣ가장 긴 바이토닉 부분 수열.py
@@ -1,25 +1,34 @@
-num_n = int(input())
-seq_n = list(map(int, input().split()))
+def solution():
 
-table_dp1 = [0] * (int(1e3) + 1)
-table_dp2 = [0] * (int(1e3) + 1)
+    dynamic_table_1 = [0] * (int(1e3) + 1)
+    dynamic_table_2 = [0] * (int(1e3) + 1)
 
-for x in range(num_n):
+    for x in range(num_n):
+        for y in range(x):
+            if seq_n[x] > seq_n[y] and dynamic_table_1[x] < dynamic_table_1[y]:
+                dynamic_table_1[x] = dynamic_table_1[y]
+        dynamic_table_1[x] += 1
 
-    for y in range(x):
-        if seq_n[x] > seq_n[y] and table_dp1[x] < table_dp1[y]:
-            table_dp1[x] = table_dp1[y]
-    table_dp1[x] += 1
+    for x in reversed(range(num_n)):
+        for y in range(num_n - 1, x, -1):
+            if seq_n[x] > seq_n[y] and dynamic_table_2[x] < dynamic_table_2[y]:
+                dynamic_table_2[x] = dynamic_table_2[y]
+        dynamic_table_2[x] += 1
 
-for x in reversed(range(num_n)):
-    for y in range(num_n - 1, x, -1):
-        if seq_n[x] > seq_n[y] and table_dp2[x] < table_dp2[y]:
-            table_dp2[x] = table_dp2[y]
-    table_dp2[x] += 1
+    sum_dynamic_table = [0] * (int(1e3) + 1)
+    []
 
-sum_dp = [0] * (int(1e3) + 1)
-for i in range(num_n):
+    for i in range(num_n):
+        sum_dynamic_table[i] = dynamic_table_1[i] + dynamic_table_2[i] - 1
 
-    sum_dp[i] = table_dp1[i] + table_dp2[i] - 1
+    answer = max(sum_dynamic_table)
+    return answer
 
-print(max(sum_dp))
+
+if __name__ == "__main__":
+
+    import sys
+
+    num_n = int(sys.stdin.readline())
+    seq_n = list(map(int, sys.stdin.readline().split()))
+
