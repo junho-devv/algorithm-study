aa440263eb81c8e17d37ac46f42ec84831ecbf3c
JIN <junho.devv@gmail.com>
2022-05-30 18:46:32 +0900

    STUDY ALGORITHM

--- BACKJOONㅣ백준/단계별로 풀어보기/15_동적 계획법 1/12865ㅣ평범한 배낭ㅣ냅색문제.py
+++ BACKJOONㅣ백준/단계별로 풀어보기/15_동적 계획법 1/12865ㅣ평범한 배낭ㅣ냅색문제.py
@@ -1,18 +1,29 @@
-in_n, in_k = map(int, input().split())
+def solution():
 
-list_item = [[0, 0]]
-for _ in range(in_n):
-    list_item.append(list(map(int, input().split())))
+    list_item = [[0, 0]]
+    for _ in range(in_n):
+        list_item.append(list(map(int, sys.stdin.readline().split())))
 
-knapsack = [[0] * (in_k + 1) for _ in range(in_n + 1)]
+    knapsack = [[0] * (in_k + 1) for _ in range(in_n + 1)]
 
-for n in range(1, in_n + 1):
-    for k in range(1, in_k + 1):
-        weight_n, value_n = list_item[n]
+    for n in range(1, in_n + 1):
+        for k in range(1, in_k + 1):
+            weight_n, value_n = list_item[n]
 
-        if k < weight_n:
-            knapsack[n][k] = knapsack[n - 1][k]
-        else:
-            knapsack[n][k] = max(value_n + knapsack[n - 1][k - weight_n], knapsack[n - 1][k])
+            if k < weight_n:
+                knapsack[n][k] = knapsack[n - 1][k]
+            else:
+                knapsack[n][k] = max(value_n + knapsack[n - 1][k - weight_n], knapsack[n - 1][k])
 
-print(knapsack[-1][-1])
+    answer = knapsack[-1][-1]
+    return answer
+
+
+if __name__ == '__main__':
+
+    import sys
+
+    in_n, in_k = map(int, sys.stdin.readline().split())
+
+    print(solution())
+
