47a90f01845a361066390878c218bd8ca36ef915
JIN <junho.devv@gmail.com>
2022-06-01 15:59:22 +0900

    STUDY ALGORITHM

--- BACKJOONㅣ백준/단계별로 풀어보기/15_동적 계획법 1/12865ㅣ평범한 배낭ㅣ냅색문제.py
+++ BACKJOONㅣ백준/단계별로 풀어보기/15_동적 계획법 1/12865ㅣ평범한 배낭ㅣ냅색문제.py
@@ -1,29 +1,28 @@
-def solution():
+def solution(item, max_weight):
 
-    list_item = [[0, 0]]
-    for _ in range(in_n):
-        list_item.append(list(map(int, sys.stdin.readline().split())))
+    knapsack = [[0] * (item + 1) for _ in range(max_weight + 1)]
 
-    knapsack = [[0] * (in_k + 1) for _ in range(in_n + 1)]
+    for n in range(1, max_weight + 1):
+        for k in range(1, item + 1):
+            weight, value = list_item[n]
 
-    for n in range(1, in_n + 1):
-        for k in range(1, in_k + 1):
-            weight_n, value_n = list_item[n]
-
-            if k < weight_n:
+            if k < weight:
                 knapsack[n][k] = knapsack[n - 1][k]
             else:
-                knapsack[n][k] = max(value_n + knapsack[n - 1][k - weight_n], knapsack[n - 1][k])
+                knapsack[n][k] = max(value + knapsack[n - 1][k - weight], knapsack[n - 1][k])
 
     answer = knapsack[-1][-1]
     return answer
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     import sys
 
     in_n, in_k = map(int, sys.stdin.readline().split())
 
-    print(solution())
+    list_item = [[0, 0]] + [list(map(int, sys.stdin.readline().split())) for _ in range(in_n)]
+
+    print(solution(in_n, in_k))
+
 
