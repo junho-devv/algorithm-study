c2cc28155eec9738d71bb7813943ef5c79465a4a
JIN <junho.devv@gmail.com>
2022-06-06 22:01:14 +0900

    STUDY ALGORITHM

--- BACKJOONㅣ백준/단계별로 풀어보기/15_동적 계획법 1/1149ㅣRGB거리.py
+++ BACKJOONㅣ백준/단계별로 풀어보기/15_동적 계획법 1/1149ㅣRGB거리.py
@@ -1,12 +1,18 @@
-input_n = int(input())
 
-table_dp = [[0, 0, 0] for _ in range(int(1e3) + 1)]
 
-for i in range(1, input_n + 1):
-    red, green, blue = map(int, input().split())
+if __name__ == "__main__":
 
-    table_dp[i][0] = min(table_dp[i - 1][1] + red, table_dp[i - 1][2] + red)
-    table_dp[i][1] = min(table_dp[i - 1][2] + green, table_dp[i - 1][0] + green)
-    table_dp[i][2] = min(table_dp[i - 1][0] + blue, table_dp[i - 1][1] + blue)
+    import sys
 
-print(min(table_dp[input_n]))
+    input_n = int(sys.stdin.readline())
+
+    table_dp = [[0, 0, 0] for _ in range(int(1e3) + 1)]
+
+    for i in range(1, input_n + 1):
+        red, green, blue = map(int, input().split())
+
+        table_dp[i][0] = min(table_dp[i - 1][1] + red, table_dp[i - 1][2] + red)
+        table_dp[i][1] = min(table_dp[i - 1][2] + green, table_dp[i - 1][0] + green)
+        table_dp[i][2] = min(table_dp[i - 1][0] + blue, table_dp[i - 1][1] + blue)
+
+    print(min(table_dp[input_n]))
