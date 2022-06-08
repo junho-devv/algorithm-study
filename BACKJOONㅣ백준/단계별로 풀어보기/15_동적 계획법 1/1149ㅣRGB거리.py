33ecd5bb84c5770bb29423efa34ac1778b518765
JIN <junho.devv@gmail.com>
2022-06-08 21:43:23 +0900

    STUDY ALGORITHM

--- BACKJOONㅣ백준/단계별로 풀어보기/15_동적 계획법 1/1149ㅣRGB거리.py
+++ BACKJOONㅣ백준/단계별로 풀어보기/15_동적 계획법 1/1149ㅣRGB거리.py
@@ -1,18 +1,16 @@
-
-
 if __name__ == "__main__":
 
     import sys
 
-    input_n = int(sys.stdin.readline())
+    in_n = int(sys.stdin.readline())
 
-    table_dp = [[0, 0, 0] for _ in range(int(1e3) + 1)]
+    dynamic_table = [[0, 0, 0] for _ in range(int(1e3) + 1)]
 
-    for i in range(1, input_n + 1):
+    for i in range(1, in_n + 1):
         red, green, blue = map(int, input().split())
 
-        table_dp[i][0] = min(table_dp[i - 1][1] + red, table_dp[i - 1][2] + red)
-        table_dp[i][1] = min(table_dp[i - 1][2] + green, table_dp[i - 1][0] + green)
-        table_dp[i][2] = min(table_dp[i - 1][0] + blue, table_dp[i - 1][1] + blue)
+        dynamic_table[i][0] = min(dynamic_table[i - 1][1] + red, dynamic_table[i - 1][2] + red)
+        dynamic_table[i][1] = min(dynamic_table[i - 1][2] + green, dynamic_table[i - 1][0] + green)
+        dynamic_table[i][2] = min(dynamic_table[i - 1][0] + blue, dynamic_table[i - 1][1] + blue)
 
-    print(min(table_dp[input_n]))
+    print(min(dynamic_table[in_n]))
