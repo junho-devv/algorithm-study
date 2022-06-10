8809f3e773b12b2025fbc4a71b954f4ab5722a9f
JIN <junho.devv@gmail.com>
2022-06-11 00:05:54 +0900

    STUDY ALGORITHM

--- BACKJOONㅣ백준/단계별로 풀어보기/15_동적 계획법 1/1904ㅣ01타일.py
+++ BACKJOONㅣ백준/단계별로 풀어보기/15_동적 계획법 1/1904ㅣ01타일.py
@@ -1,13 +1,23 @@
-import sys
+def solution():
+
+    dynamic_table = [0] * (int(1e6) + 1)
+    table_dp = [0] * (int(1e6) + 1)
+
+    table_dp[1], table_dp[2] = 1, 2
+
+    for i in range(3, input_n + 1):
+        table_dp[i] = (table_dp[i - 1] + table_dp[i - 2]) % 15746
+
+    print(table_dp[input_n])
 
 
-input_n = int(sys.stdin.readline())
+    answer = 0
+    return answer
 
-table_dp = [0] * (int(1e6) + 1)
+if __name__ == "__main__":
 
-table_dp[1], table_dp[2] = 1, 2
+    import sys
 
-for i in range(3, input_n + 1):
-    table_dp[i] = (table_dp[i - 1] + table_dp[i - 2]) % 15746
+    in_n = int(sys.stdin.readline())
 
-print(table_dp[input_n])
+    print(solution(in_n))
