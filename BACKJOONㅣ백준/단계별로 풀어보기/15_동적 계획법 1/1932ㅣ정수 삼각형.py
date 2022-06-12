54fa966c184db8498a26bc0f96d08c2cd2d33ed8
JIN <junho.devv@gmail.com>
2022-06-13 00:03:16 +0900

    STUDY ALGORITHM

--- BACKJOONㅣ백준/단계별로 풀어보기/15_동적 계획법 1/1932ㅣ정수 삼각형.py
+++ BACKJOONㅣ백준/단계별로 풀어보기/15_동적 계획법 1/1932ㅣ정수 삼각형.py
@@ -1,15 +1,22 @@
-import sys
+def solution(N, triangle):
+    for i in range(1, N):
+        for j in range(len(triangle[i])):
+            if j == 0:
+                triangle[i][j] += triangle[i - 1][j]
+            elif j == len(triangle[i]) - 1:
+                triangle[i][j] += triangle[i - 1][j - 1]
+            else:
+                triangle[i][j] += max(triangle[i - 1][j], triangle[i - 1][j - 1])
 
-size_n = int(sys.stdin.readline())
-triangle_n = [list(map(int, sys.stdin.readline().split())) for _ in range(size_n)]
+    answer = max(triangle[N - 1])
+    return answer
 
-for i in range(1, size_n):
-    for j in range(len(triangle_n[i])):
-        if j == 0:
-            triangle_n[i][j] += triangle_n[i - 1][j]
-        elif j == len(triangle_n[i]) - 1:
-            triangle_n[i][j] += triangle_n[i - 1][j - 1]
-        else:
-            triangle_n[i][j] += max(triangle_n[i - 1][j], triangle_n[i - 1][j - 1])
 
-print(max(triangle_n[size_n - 1]))
+if __name__ == "__main__":
+
+    import sys
+
+    in_n = int(sys.stdin.readline())
+    in_t = [list(map(int, sys.stdin.readline().split())) for _ in range(in_n)]
+
+    print(solution(in_n, in_t))
