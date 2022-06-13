ccd9d6847df59cab6f6aead38f2c5a188bf4a284
JIN <junho.devv@gmail.com>
2022-06-13 14:35:06 +0900

    STUDY ALGORITHM

--- BACKJOONㅣ백준/단계별로 풀어보기/15_동적 계획법 1/2156ㅣ포도주 시식.py
+++ BACKJOONㅣ백준/단계별로 풀어보기/15_동적 계획법 1/2156ㅣ포도주 시식.py
@@ -1,16 +1,22 @@
-num_n = int(input())
+def solution(N, wines):
 
-amount_n = [0] * (int(1e4) + 1)
-table_dp = [0] * (int(1e4) + 1)
+    dynamic_table = [0] * (10 ** 4 + 1)
+    dynamic_table[1] = wines[1]
+    dynamic_table[2] = wines[1] + wines[2]
 
-for i in range(1, num_n + 1):
-    amount_n[i] = int(input())
+    for i in range(3, N + 1):
+        dynamic_table[i] = max(dynamic_table[i - 3] + wines[i - 1] + wines[i], dynamic_table[i - 2] + wines[i],
+                               dynamic_table[i - 1])
 
-table_dp[1] = amount_n[1]
-table_dp[2] = amount_n[1] + amount_n[2]
+    answer = dynamic_table[N]
+    return answer
 
-for i in range(3, num_n + 1):
 
-    table_dp[i] = max(table_dp[i - 3] + amount_n[i - 1] + amount_n[i], table_dp[i - 2] + amount_n[i], table_dp[i - 1])
+if __name__ == "__main__":
 
-print(table_dp[num_n])
+    import sys
+
+    in_n = int(sys.stdin.readline())
+    in_w = [0] + [int(sys.stdin.readline()) for _ in range(in_n)]
+
+    print(solution(in_n, in_w))
