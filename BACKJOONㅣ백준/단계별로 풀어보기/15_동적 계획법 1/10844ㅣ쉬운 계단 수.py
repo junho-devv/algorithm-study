285566d29dfd97dda27443223d402d7189750096
JIN <junho.devv@gmail.com>
2022-06-16 11:41:23 +0900

    STUDY ALGORITHM

--- BACKJOONㅣ백준/단계별로 풀어보기/15_동적 계획법 1/10844ㅣ쉬운 계단 수.py
+++ BACKJOONㅣ백준/단계별로 풀어보기/15_동적 계획법 1/10844ㅣ쉬운 계단 수.py
@@ -1,17 +1,25 @@
-length_n = int(input())
+def solution(N):
 
-table_dp = [[0] * 10 for _ in range(101)]
-for i in range(1, 10):
-    table_dp[1][i] = 1
+    dynamic_table = [[0] * 10 for _ in range(101)]
 
-for i in range(2, length_n + 1):
-    for j in range(10):
+    for idx_1 in range(2, N + 1):
+        for idx_2 in range(10):
 
-        if j == 0:
-            table_dp[i][j] = table_dp[i - 1][1]
-        elif j == 9:
-            table_dp[i][j] = table_dp[i - 1][8]
-        else:
-            table_dp[i][j] = table_dp[i - 1][j - 1] + table_dp[i - 1][j + 1]
+            if idx_2 == 0:
+                dynamic_table[idx_1][idx_2] = dynamic_table[idx_1 - 1][1]
+            elif idx_2 == 9:
+                dynamic_table[idx_1][idx_2] = dynamic_table[idx_1 - 1][8]
+            else:
+                dynamic_table[idx_1][idx_2] = dynamic_table[idx_1 - 1][idx_2 - 1] + dynamic_table[idx_1 - 1][idx_2 + 1]
 
-print(sum(table_dp[length_n]) % int(1e9))
+    answer = sum(dynamic_table[N]) % 10 ** 9
+    return answer
+
+
+if __name__ == "__main__":
+
+    import sys
+
+    in_n = int(sys.stdin.readline())
+
+    print(solution(in_n))
