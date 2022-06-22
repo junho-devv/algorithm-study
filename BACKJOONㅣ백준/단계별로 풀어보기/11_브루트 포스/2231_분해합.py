44ebb6c2fe15df8c7921f09f15e3098cbdb11299
JIN <junho.devv@gmail.com>
2022-06-22 14:08:47 +0900

    STUDY ALGORITHM

--- BACKJOONㅣ백준/단계별로 풀어보기/11_브루트 포스/2231_분해합.py
+++ BACKJOONㅣ백준/단계별로 풀어보기/11_브루트 포스/2231_분해합.py
@@ -1,16 +1,12 @@
-def solution():
-    # 자연수 N(input_n) 입력받기
-    input_n = int(input())
-    # 자연수 N(input_n)의 생성자(answer)
+def solution(n):
+
     answer = 0
 
-    for i in range(1, input_n):
+    for i in range(1, n):
 
         int_n = i
-
-        # 임의의 수의 자기자신과 각 자리수의 합(sum_n)
         sum_n = i
-        # sum_n 구하기
+
         while True:
 
             if i // 10 == 0:
@@ -21,11 +17,17 @@ def solution():
 
             i = i // 10
 
-        if sum_n == input_n:
+        if sum_n == n:
             answer = int_n
             break
 
     print(answer)
 
 
-solution()
+if __name__ == "__main__":
+
+    import sys
+
+    in_n = int(sys.stdin.readline())
+
+    print(solution(in_n))
