202fa991405b7eba8e29dc8567a4f5dd7750a0ed
JIN <junho.devv@gmail.com>
2022-06-23 18:38:24 +0900

    STUDY ALGORITHM

--- BACKJOONㅣ백준/단계별로 풀어보기/11_브루트 포스/7568_덩치.py
+++ BACKJOONㅣ백준/단계별로 풀어보기/11_브루트 포스/7568_덩치.py
@@ -1,26 +1,31 @@
 def solution():
-    # 전체 사람의 수(num_p) 입력받기
-    num_p = int(input())
-    # 전체 사람의 몸무게와 키를 저장할 배열(arr_p)
-    arr_p = []
 
-    for _ in range(num_p):
-
-        weight_x, height_x = map(int, input().split())
-
-        arr_p.append([weight_x, height_x, 1])
-
-    for a in range(num_p):
-        for b in range(num_p):
+    for p_1 in range(in_n):
+        for p_2 in range(in_n):
             # 자기자신과 비교는 생략
-            if a == b:
+            if p_1 == p_2:
                 continue
 
-            if arr_p[a][0] < arr_p[b][0] and arr_p[a][1] < arr_p[b][1]:
-                arr_p[a][2] += 1
+            if info_people[p_1][0] < info_people[p_2][0] and info_people[p_1][1] < info_people[p_2][1]:
+                info_people[p_1][2] += 1
     # 결과 출력하기
-    for i in arr_p:
-        print(i[2], end=' ')
+    for _1, _2, rank in info_people:
+        print(rank, end=' ')
 
 
-solution()
+if __name__ == "__main__":
+
+    import sys
+
+    in_n = int(sys.stdin.readline())
+
+    # 전체 사람의 몸무게와 키를 저장할 배열(arr_p)
+    info_people = []
+
+    for _ in range(in_n):
+
+        weight, height = map(int, sys.stdin.readline().split())
+
+        info_people.append([weight, height, 1])
+
+    solution()
