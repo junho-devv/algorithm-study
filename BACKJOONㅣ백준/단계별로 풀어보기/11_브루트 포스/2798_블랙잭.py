4e4800d0a08729fe42884e48762c7532b4154479
JIN <junho.devv@gmail.com>
2022-06-24 15:58:15 +0900

    STUDY ALGORITHM

--- BACKJOONㅣ백준/단계별로 풀어보기/11_브루트 포스/2798_블랙잭.py
+++ BACKJOONㅣ백준/단계별로 풀어보기/11_브루트 포스/2798_블랙잭.py
@@ -1,7 +1,6 @@
-from itertools import combinations
-
-
 def solution():
+
+    from itertools import combinations
 
     input_n, input_m = map(int, input().split())
 
@@ -22,4 +21,28 @@ def solution():
     print(sum(answer))
 
 
-solution()
+def solution_2(N, M, numbers):
+
+    answer = 0
+
+    for idx_1 in range(N):
+        for idx_2 in range(idx_1 + 1, N):
+            for idx_3 in range(idx_2 + 1, N):
+
+                if numbers[idx_1] + numbers[idx_2] + numbers[idx_3] > M:
+                    continue
+                else:
+                    answer = max(answer, numbers[idx_1] + numbers[idx_2] + numbers[idx_3])
+
+    return answer
+
+
+if __name__ == "__main__":
+
+    import sys
+
+    in_n, in_m = map(int, sys.stdin.readline().split())
+    in_l = list(map(int, sys.stdin.readline().split()))
+
+    print(solution_2(in_n, in_m, in_l))
+
