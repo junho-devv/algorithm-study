78b8f603da30a8c89f4ad13d0857b81a879851f3
JIN <junho.devv@gmail.com>
2022-06-26 13:02:25 +0900

    STUDY ALGORITHM

--- BACKJOONㅣ백준/단계별로 풀어보기/12_정렬/1181_단어정렬.py
+++ BACKJOONㅣ백준/단계별로 풀어보기/12_정렬/1181_단어정렬.py
@@ -1,14 +1,13 @@
 def solution():
 
-    num_x = int(input())
     arr_x = []
 
-    for _ in range(num_x):
-        # 입력할 단어(word_x), 해당 단어의 길이(len_x)
-        word_x = input()
-        len_x = len(word_x)
+    for _ in range(in_n):
+        # 입력할 단어(word), 해당 단어의 길이(len_word)
+        word = sys.stdin.readline()
+        len_word = len(word)
 
-        arr_x.append((word_x, len_x))
+        arr_x.append((word, len_word))
 
     arr_y = list(set(arr_x))
     arr_y.sort(key=lambda x: (x[1], x[0]))
@@ -17,4 +16,9 @@ def solution():
         print(i[0])
 
 
-solution()
+if __name__ == "__main__":
+
+    import sys
+
+    in_n = int(sys.stdin.readline())
+    solution()
