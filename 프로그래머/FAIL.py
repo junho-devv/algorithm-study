04c03d38825b07fdd404f1c95fe8a203152f80d8
JIN <junho.devv@gmail.com>
2022-06-30 15:27:39 +0900

    STUDY ALGORITHM

--- 프로그래머/FAIL.py
+++ 프로그래머/FAIL.py
@@ -7,12 +7,14 @@ def solution(num, stages):
 
     a_result = []
     c_num = len(stages)
+
     for i in range(1, num + 1):
         a_failure = temp_list[i] / c_num
         c_num -= temp_list[i]
         a_result.append((a_failure, i))
 
     a_result.sort(reverse=True, key=lambda x: x[0])
+
     for a in a_result:
         answer.append(a[1])
 
