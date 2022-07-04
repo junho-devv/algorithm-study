4eeee863c88029fa15933d932d4842b136d457af
JIN <junho.devv@gmail.com>
2022-07-04 13:33:44 +0900

    STUDY ALGORITHM

--- 자료구조/recursive call.py
+++ 자료구조/recursive call.py
@@ -1,9 +1,18 @@
-def factorial(n):
+def factorial_recursive(n):
 
     if n == 1:
         return 1
 
-    return n * factorial(n-1)
+    return n * factorial_recursive(n-1)
+
+
+def factorial_loop(n):
+
+    answer = 1
+    for idx in range(1, n + 1):
+        answer *= idx
+
+    return answer
 
 
 if __name__ == "__main__":
@@ -12,4 +21,4 @@ if __name__ == "__main__":
 
     in_n = int(sys.stdin.readline())
 
-    print(factorial(5))
+    print(factorial_loop(in_n))
