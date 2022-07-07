9a4bcc7817910d2d9ff0d2be6d1dfe4076f5d8e7
JIN <junho.devv@gmail.com>
2022-07-07 15:10:12 +0900

    STUDY ALGORITHM

--- 자료구조/DATA STRUCTUREㅣQueue.py
+++ 자료구조/DATA STRUCTUREㅣQueue.py
@@ -1,11 +1,7 @@
-from collections import deque
 import sys
 
 
-que_1 = []
-for _ in range(3):
-    in_ = int(sys.stdin.readline())
-    que_1.append(in_)
+que_1 = [_ for _ in range(10)]
 que_1.pop(0)
 
-print(que_1)
+print("출력", que_1)
