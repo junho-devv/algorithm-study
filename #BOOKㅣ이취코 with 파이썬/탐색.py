747146c9f978f9daad55f3dd6bbc8e4f94c083c5
JIN <junho.devv@gmail.com>
2022-07-18 13:09:02 +0900

    STUDY ALGORITHM

--- #BOOKㅣ이취코 with 파이썬/탐색.py
+++ #BOOKㅣ이취코 with 파이썬/탐색.py
@@ -6,46 +6,30 @@ def sequential_search(num, tar, arr):
 
 def binary_search(arr, tar, start, end):
 
-    # if start > end:
-    #     return None
-    #
-    # mid = (start + end) // 2
-    #
-    # if arr[mid] == tar:
-    #     return mid + 1
-    # elif arr[mid] > tar:
-    #     return binary_search(arr, tar, start, mid - 1)
-    # else:
-    #     return binary_search(arr, tar, mid + 1, end)
-
     while start <= end:
 
         mid = (start + end) // 2
 
         if arr[mid] == tar:
             return mid + 1
+
         elif arr[mid] > tar:
             end = mid - 1
+
         else:
             start = mid + 1
 
     return None
 
 
-# input_data = input().split()
-# n = int(input_data[0])
-# target = input_data[1]
-#
-# aList = input().split()
-#
-# print(sequential_search(n, target, aList))
+if __name__ == "__main__":
 
-n, target = map(int, input().split())
-aList = list(map(int, input().split()))
+    n, target = map(int, input().split())
+    aList = list(map(int, input().split()))
 
-aResult = binary_search(aList, target, 0, n - 1)
+    aResult = binary_search(aList, target, 0, n - 1)
 
-if aResult is None:
-    print("해당 값이 없습니다.")
-else:
-    print(aResult)
+    if aResult is None:
+        print("해당 값이 없습니다.")
+    else:
+        print(aResult)
