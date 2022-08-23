5c764c059714c838f2bd169ddd32c15c0cabfac2
JIN <junho.devv@gmail.com>
2022-08-23 23:55:35 +0900

    STUDY ALGORITHM

--- #BOOKㅣ이취코 with 파이썬/CHAPTER 03ㅣ그리디/greedy003.py
+++ #BOOKㅣ이취코 with 파이썬/CHAPTER 03ㅣ그리디/greedy003.py
@@ -1,20 +1,19 @@
-n, m = map(int, input().split())
-
-result = 0
-
-for x in range(n):
-    
-    data = list(map(int, input().split()))
-    min_value = float('inf')
-
-    for y in data:
-
-        min_value = min(y, min_value)
-    
-    result = max(result, min_value)
-
-print(result)
-
 if __name__ == "__main__":
 
     import sys
+
+    n, m = map(int, sys.stdin.readline().split())
+
+    result = 0
+
+    for x in range(n):
+
+        data = list(map(int, input().split()))
+        min_value = float('inf')
+
+        for y in data:
+            min_value = min(y, min_value)
+
+        result = max(result, min_value)
+
+    print(result)
