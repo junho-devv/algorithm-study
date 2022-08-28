199fa34fac052861c464a7a8875018fb1dfa2e7a
JIN <junho.devv@gmail.com>
2022-08-28 21:44:27 +0900

    STUDY ALGORITHM

--- #BOOKㅣ이취코 with 파이썬/CHAPTER 03ㅣ그리디/greedy004.py
+++ #BOOKㅣ이취코 with 파이썬/CHAPTER 03ㅣ그리디/greedy004.py
@@ -1,17 +1,25 @@
-n, k = map(int, input().split())
+def solution(n, k):
+    count = 0
 
-count = 0
+    while True:
+        if n == 1:
+            break
 
-while True:
-    if n == 1:
-        break
+        if n % k == 0:
+            n /= k
+            count += 1
 
-    if n % k == 0:
-        n /= k
-        count += 1
-    
-    else:
-        n -= 1
-        count += 1
+        else:
+            n -= 1
+            count += 1
 
-print(count)
+    return count
+
+
+if __name__ == "__main__":
+
+    import sys
+
+    in_n, in_k = map(int, sys.stdin.readline().split())
+
+    print(solution(in_n, in_k))
