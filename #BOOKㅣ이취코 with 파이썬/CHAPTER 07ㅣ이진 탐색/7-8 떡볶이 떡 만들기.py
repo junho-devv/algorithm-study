cbc041d79e10b90b92e843c29ed1cdd6b9a26388
JIN <junho.devv@gmail.com>
2022-11-04 12:42:30 +0900

    STUDY ALGORITHM

---
+++ #BOOKㅣ이취코 with 파이썬/CHAPTER 07ㅣ이진 탐색/7-8 떡볶이 떡 만들기.py
@@ -0,0 +1,29 @@
+def search_height(array, target, start, end):
+
+    answer = 0
+
+    while start <= end:
+        mid = (start + end) // 2
+
+        leftover = 0
+        for h in array:
+            if h > mid:
+                leftover += h - mid
+
+        if leftover < target:
+            end = mid - 1
+        else:
+            start = mid + 1
+            answer = mid
+
+    print(answer)
+
+
+if __name__ == "__main__":
+
+    import sys
+
+    in_n, in_m = map(int, sys.stdin.readline().split())
+    in_h = list(map(int, sys.stdin.readline().split()))
+
+    print(search_height(in_h, in_m, 0, max(in_h)))
