fcc95f0bbde64913f5d57b5c8264704350d65185
JIN <junho.devv@gmail.com>
2022-12-14 19:40:08 +0900

    STUDY ALGORITHM

--- #BOOKㅣ이취코 with 파이썬/MST_ex002.py
+++ #BOOKㅣ이취코 with 파이썬/MST_ex002.py
@@ -1,6 +1,3 @@
-import sys
-
-
 def find_center_city(graph, x_house):
 
     if graph[x_house] != x_house:
@@ -20,49 +17,33 @@ def connect_two_houses(graph, x_house, y_house):
         graph[x] = y
 
 
-h_num, r_num = map(int, sys.stdin.readline().split())
+if __name__ == "__main__":
 
-city_map = [0] * (h_num + 1)
+    import sys
 
-for i in range(1, h_num + 1):
-    city_map[i] = i
+    h_num, r_num = map(int, sys.stdin.readline().split())
 
-city_road = []
+    city_map = [0] * (h_num + 1)
+    for i in range(1, h_num + 1):
+        city_map[i] = i
 
-for _ in range(r_num):
-    a_house, b_house, m_cost = map(int, sys.stdin.readline().split())
-    city_road.append([m_cost, a_house, b_house])
+    city_road = []
+    for _ in range(r_num):
+        a_house, b_house, m_cost = map(int, sys.stdin.readline().split())
+        city_road.append([m_cost, a_house, b_house])
+    city_road.sort()
 
-city_road.sort()
+    min_cost = list()
 
-min_cost = []
+    for i in range(r_num):
+        a_house = find_center_city(city_map, city_road[i][1])
+        b_house = find_center_city(city_map, city_road[i][2])
 
-# for i in range(r_num):
-#     print(city_road[i][0], "+", city_road[i][1], "+", city_road[i][2])
+        if a_house != b_house:
+            connect_two_houses(city_map, city_road[i][1], city_road[i][2])
+            min_cost.append(city_road[i][0])
 
-for i in range(r_num):
+    max_cost = max(min_cost)
 
-    a_house = find_center_city(city_map, city_road[i][1])
-    b_house = find_center_city(city_map, city_road[i][2])
-
-    if a_house != b_house:
-        connect_two_houses(city_map, city_road[i][1], city_road[i][2])
-        # min_cost += city_road[i][0]
-        min_cost.append(city_road[i][0])
-
-# for edge in city_road:
-#     cost, a, b = edge
-#
-#     if find_center_city(city_map, a) != find_center_city(city_map, b):
-#         connect_two_houses(city_map, a, b)
-#         min_cost += cost
-
-max_cost = max(min_cost)
-
-print(city_map)
-
-print(sum(min_cost) - max_cost)
-
-
-
-
+    print(city_map)
+    print(sum(min_cost) - max_cost)
