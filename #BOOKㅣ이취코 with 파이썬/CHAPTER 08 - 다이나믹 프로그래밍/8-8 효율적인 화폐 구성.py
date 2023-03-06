62c2a132de1ca982a8a88bdab5ccb8c7e2d5553b
JIN <junho.devv@gmail.com>
2023-03-06 12:15:40 +0900

    STUDY ALGORITHM

---
+++ #BOOKㅣ이취코 with 파이썬/CHAPTER 08 - 다이나믹 프로그래밍/8-8 효율적인 화폐 구성.py
@@ -0,0 +1,21 @@
+import sys
+
+MAX = int(1e4) + 1
+
+in_n, in_m = map(int, sys.stdin.readline().split())
+
+mv = list()
+for n in range(in_n):
+    mv.append(int(sys.stdin.readline()))
+
+dpt = [0] + [MAX] * in_m
+
+for v in mv:
+    for m in range(v, in_m + 1):
+        if dpt[m - v] != MAX:
+            dpt[m] = min(dpt[m - v] + 1, dpt[m])
+
+if dpt[in_m] == MAX:
+    print(-1)
+else:
+    print(dpt[in_m])
