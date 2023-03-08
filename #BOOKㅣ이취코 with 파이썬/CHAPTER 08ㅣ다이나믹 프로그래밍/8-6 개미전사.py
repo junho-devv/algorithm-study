66dcb4ac4d7571cd4a2e21d8d374b2749593f0b8
JIN <junho.devv@gmail.com>
2023-03-08 12:43:12 +0900

    STUDY ALGORITHM

--- #BOOKㅣ이취코 with 파이썬/CHAPTER 08ㅣ다이나믹 프로그래밍/8-6 개미전사.py
+++ #BOOKㅣ이취코 with 파이썬/CHAPTER 08ㅣ다이나믹 프로그래밍/8-6 개미전사.py
@@ -5,7 +5,7 @@ in_n = int(sys.stdin.readline())
 food_storage = list(map(int, sys.stdin.readline().split()))
 
 dpt = [0] * int(100)
-dpt[0], dpt[1] = food_storage[0], food_storage[1]
+dpt[0], dpt[1] = food_storage[0], max(food_storage[0], food_storage[1])
 
 for n in range(2, in_n):
     dpt[n] = max(dpt[n - 1], dpt[n - 2] + food_storage[n])
